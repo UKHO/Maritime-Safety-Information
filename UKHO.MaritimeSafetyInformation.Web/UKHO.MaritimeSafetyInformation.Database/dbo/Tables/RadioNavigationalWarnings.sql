d2b065736b2f4ecac691c0edc460259a6e074b40
Ashish Shelar <ashish15251@mastek.com>
2022-04-19 11:10:39 +0530

    AB#46363 database project created

---
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Database/dbo/Tables/RadioNavigationalWarnings.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[RadioNavigationalWarnings] (
+    [id]             INT           NOT NULL,
+    [WarningType]    VARCHAR (32)  NULL,
+    [Reference]      VARCHAR (32)  NULL,
+    [DateTimeGroup]  DATETIME      NULL,
+    [Description]    VARCHAR (256) NULL,
+    [Text]           VARCHAR (MAX) NULL,
+    [ExpiryDate]     DATETIME      NULL,
+    [Approvalstatus] BIT           NULL,
+    [IsDeleted]      BIT           NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
