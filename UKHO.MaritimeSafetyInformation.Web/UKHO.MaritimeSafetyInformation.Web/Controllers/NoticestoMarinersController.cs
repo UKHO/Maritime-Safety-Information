59ab75bc413688dc5b03d2e31c66c2b8ecd5a769
Avinash15515 <avinash.patil@mastek.com>
2022-04-19 19:00:02 +0530

    45735 AB# - Added JavaScript logic to fill dropdowns

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/Controllers/NoticestoMarinersController.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/Controllers/NoticestoMarinersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
 using UKHO.FileShareClient;
 using UKHO.FileShareClient.Models;
 using UKHO.MaritimeSafetyInformation.Web.Models;
@@ -23,10 +24,22 @@ namespace UKHO.MaritimeSafetyInformation.Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("~/Views/NoticestoMariners/ShowWeeklyFiles.cshtml");
         }
 
-        public async Task<IActionResult> ShowWeeklyFilesAsync()
+        public IActionResult LoadYears()
+        {
+
+            return Json(GetPastYears());
+        }
+
+        public IActionResult LoadWeeks(int year)
+        {
+
+            return Json(GetAllWeeksofYear(year));
+        }
+
+        public async Task<IActionResult> ShowWeeklyFilesAsync(int year, int week)
         {
             string accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyIsImtpZCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyJ9.eyJhdWQiOiI4MDViZTAyNC1hMjA4LTQwZmItYWI2Zi0zOTljMjY0N2QzMzQiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC85MTM0Y2E0OC02NjNkLTRhMDUtOTY4YS0zMWE0MmYwYWVkM2UvIiwiaWF0IjoxNjUwMzY4NTg2LCJuYmYiOjE2NTAzNjg1ODYsImV4cCI6MTY1MDM3MzQxMCwiYWNyIjoiMSIsImFpbyI6IkFWUUFxLzhUQUFBQXVmOGdiOW9XL1hlUDFuQWZvLzM3d3ZNNDN0QW8yS3pYYlNWSENEVytZZ1pIeTArSVdwbE0wSDZXM005bzlLUUhXTERXMm9GdE5wMHAzblhPRHVSNjNCc2V5cDFwOFN2L3FBMnRpYUo5ZXpNPSIsImFtciI6WyJwd2QiLCJyc2EiXSwiYXBwaWQiOiI4MDViZTAyNC1hMjA4LTQwZmItYWI2Zi0zOTljMjY0N2QzMzQiLCJhcHBpZGFjciI6IjAiLCJlbWFpbCI6Im1vaGFtbWUxNTMxNUBtYXN0ZWsuY29tIiwiaWRwIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvYWRkMWM1MDAtYTZkNy00ZGJkLWI4OTAtN2Y4Y2I2ZjdkODYxLyIsImlwYWRkciI6IjIyMy4xODkuNTEuMjEiLCJuYW1lIjoiTW9oYW1tZWQgS2hhbiIsIm9pZCI6IjAxMzA1NWNiLWVkNjItNDZkMi1hZGU4LTM4ZmNjZDY0MGFhNiIsInJoIjoiMC5BVk1BU01vMGtUMW1CVXFXaWpHa0x3cnRQaVRnVzRBSW92dEFxMjg1bkNaSDB6UUNBQTQuIiwicm9sZXMiOlsiQmF0Y2hDcmVhdGUiXSwic2NwIjoiVXNlci5SZWFkIiwic3ViIjoiZ2RwR3VBN3VTZk9HY0RuS2VmZk41MXZBUDZXa2hKNldxbHdOaVpSMk9qNCIsInRpZCI6IjkxMzRjYTQ4LTY2M2QtNGEwNS05NjhhLTMxYTQyZjBhZWQzZSIsInVuaXF1ZV9uYW1lIjoibW9oYW1tZTE1MzE1QG1hc3Rlay5jb20iLCJ1dGkiOiJ0NkM1UVpJMVhFbW1seXlORU9NVEFBIiwidmVyIjoiMS4wIn0.e6Cl0Nk0pFogQtqIRygDrCpzYFWeRfF0aSOwWAxVGQUClfV5mYN34jGBR3fUt4flbVH3mYe4rovJ5e6Bjrxu5tpMRNH1vOS8CmCPlAp8NHn8MYncAcDyCSoGl-kqHyRYlZJPpw8TryCACKUKd7-46ZnILLdMiLaZdNyI9Q3ISfM8KEL5f_Sn56m_qktDh8kAtxOXZ_fuEhIpYf3B07RUDUE1wCzMIwc4FItZ20uN_3UptbwCNFkJPdWGeJGUc5sOyu4oeJOvp2XT5ndYBYfLgOfxajF_K1z0_0w7NwZeZpQ6E2a33mzll26QsBukOWIigJZbwUTTmMWexJWxr1Nwdg";
             List<ShowFilesResponseModel> listFiles = await nMDataService.GetBatchDetailsFiles(2022, 15, accessToken);
@@ -40,6 +53,45 @@ namespace UKHO.MaritimeSafetyInformation.Web.Controllers
             string accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyIsImtpZCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyJ9.eyJhdWQiOiI4MDViZTAyNC1hMjA4LTQwZmItYWI2Zi0zOTljMjY0N2QzMzQiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC85MTM0Y2E0OC02NjNkLTRhMDUtOTY4YS0zMWE0MmYwYWVkM2UvIiwiaWF0IjoxNjUwMzY4NTg2LCJuYmYiOjE2NTAzNjg1ODYsImV4cCI6MTY1MDM3MzQxMCwiYWNyIjoiMSIsImFpbyI6IkFWUUFxLzhUQUFBQXVmOGdiOW9XL1hlUDFuQWZvLzM3d3ZNNDN0QW8yS3pYYlNWSENEVytZZ1pIeTArSVdwbE0wSDZXM005bzlLUUhXTERXMm9GdE5wMHAzblhPRHVSNjNCc2V5cDFwOFN2L3FBMnRpYUo5ZXpNPSIsImFtciI6WyJwd2QiLCJyc2EiXSwiYXBwaWQiOiI4MDViZTAyNC1hMjA4LTQwZmItYWI2Zi0zOTljMjY0N2QzMzQiLCJhcHBpZGFjciI6IjAiLCJlbWFpbCI6Im1vaGFtbWUxNTMxNUBtYXN0ZWsuY29tIiwiaWRwIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvYWRkMWM1MDAtYTZkNy00ZGJkLWI4OTAtN2Y4Y2I2ZjdkODYxLyIsImlwYWRkciI6IjIyMy4xODkuNTEuMjEiLCJuYW1lIjoiTW9oYW1tZWQgS2hhbiIsIm9pZCI6IjAxMzA1NWNiLWVkNjItNDZkMi1hZGU4LTM4ZmNjZDY0MGFhNiIsInJoIjoiMC5BVk1BU01vMGtUMW1CVXFXaWpHa0x3cnRQaVRnVzRBSW92dEFxMjg1bkNaSDB6UUNBQTQuIiwicm9sZXMiOlsiQmF0Y2hDcmVhdGUiXSwic2NwIjoiVXNlci5SZWFkIiwic3ViIjoiZ2RwR3VBN3VTZk9HY0RuS2VmZk41MXZBUDZXa2hKNldxbHdOaVpSMk9qNCIsInRpZCI6IjkxMzRjYTQ4LTY2M2QtNGEwNS05NjhhLTMxYTQyZjBhZWQzZSIsInVuaXF1ZV9uYW1lIjoibW9oYW1tZTE1MzE1QG1hc3Rlay5jb20iLCJ1dGkiOiJ0NkM1UVpJMVhFbW1seXlORU9NVEFBIiwidmVyIjoiMS4wIn0.e6Cl0Nk0pFogQtqIRygDrCpzYFWeRfF0aSOwWAxVGQUClfV5mYN34jGBR3fUt4flbVH3mYe4rovJ5e6Bjrxu5tpMRNH1vOS8CmCPlAp8NHn8MYncAcDyCSoGl-kqHyRYlZJPpw8TryCACKUKd7-46ZnILLdMiLaZdNyI9Q3ISfM8KEL5f_Sn56m_qktDh8kAtxOXZ_fuEhIpYf3B07RUDUE1wCzMIwc4FItZ20uN_3UptbwCNFkJPdWGeJGUc5sOyu4oeJOvp2XT5ndYBYfLgOfxajF_K1z0_0w7NwZeZpQ6E2a33mzll26QsBukOWIigJZbwUTTmMWexJWxr1Nwdg";
             byte[] fileBytes = await nMDataService.DownloadFssFileAsync(batchId, filename, accessToken);
             return File(fileBytes, "application/octet-stream", filename);
+        }
+
+        private List<KeyValuePair<string, string>> GetPastYears()
+        {
+            List<KeyValuePair<string, string>> years = new List<KeyValuePair<string, string>>();
+            years.Add(new KeyValuePair<string, string>("Year", ""));
+            for (int i = 0; i < 3; i++)
+            {
+                string year = (DateTime.Now.Year - i).ToString();
+                years.Add(new KeyValuePair<string, string>(year, year));
+            }
+            return years;
+        }
+        private List<KeyValuePair<string, string>> GetAllWeeksofYear(int year)
+        {
+            List<KeyValuePair<string, string>> weeks = new List<KeyValuePair<string, string>>();
+            weeks.Add(new KeyValuePair<string, string>("Week Number", ""));
+
+            DateTimeFormatInfo dfi = DateTimeFormatInfo.CurrentInfo;
+            DateTime lastdate;
+            if (DateTime.Now.Year == year)
+            {
+                lastdate = new DateTime(year, DateTime.Now.Month, DateTime.Now.Day);
+            }
+            else
+            {
+                lastdate = new DateTime(year, 12, 31);
+            }
+            Calendar cal = dfi.Calendar;
+
+            int totalWeeks = cal.GetWeekOfYear(lastdate, dfi.CalendarWeekRule,
+                                                dfi.FirstDayOfWeek);
+
+            for (int i = 0; i < totalWeeks; i++)
+            {
+                string week = (totalWeeks - i).ToString();
+                weeks.Add(new KeyValuePair<string, string>(week, week));
+            }
+            return weeks;
         }
     }
 }
---
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/wwwroot/js/Custom/NoticesToMariners.js
@@ -0,0 +1,82 @@
+﻿//Document ready 
+$(function () {
+    LoadYears();
+    $("#ddlYears").change(function () {
+        LoadWeeks($("#ddlYears").val());
+    });
+    $("#ddlWeeks").change(function () {
+        ShowWeeklyFilesAsync();
+    });
+});
+
+function LoadYears() {
+    $.ajax({
+        url: '/NoticestoMariners/LoadYears',
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            $('#ddlYears').empty();
+            $.each(data, function (i, data) {
+                var div_data = "<option value=" + data.value + ">" + data.key + "</option>";
+                $(div_data).appendTo('#ddlYears');
+                let curYear = new Date().getFullYear()
+                $('#ddlYears').val(curYear);
+                LoadWeeks(curYear);
+
+            });
+        },
+        error: function (error) {
+            console.log(`Error ${error}`);
+        }
+    });
+}
+
+function LoadWeeks(selectedYear) {
+    if (selectedYear != "") {
+        $.ajax({
+            url: '/NoticestoMariners/LoadWeeks',
+            type: "POST",
+            data: {
+                year: parseInt(selectedYear)
+            },
+            dataType: "json",
+            success: function (data) {
+                $('#ddlWeeks').empty();
+                $.each(data, function (i, data) {
+                    var div_data = "<option value=" + data.value + ">" + data.key + "</option>";
+                    $(div_data).appendTo('#ddlWeeks');
+                });
+            },
+            error: function (error) {
+                console.log(`Error ${error}`);
+            }
+        });
+    }
+}
+
+function ShowWeeklyFilesAsync() {
+    let selectedYear = $('#ddlYears').val();
+    let selectedWeek = $('#ddlWeeks').val();
+    let datas = JSON.stringify({
+        year: selectedYear,
+        week: selectedWeek
+    });
+    if (selectedYear != "" && selectedWeek != "") {
+        $.ajax({
+            url: '/NoticestoMariners/ShowWeeklyFiles',
+            type: "POST",
+            data: {
+                year: parseInt(selectedYear),
+                week: parseInt(selectedWeek)
+            },
+            success: function (data) {
+
+                console.log(data);
+            },
+            error: function (error) {
+                console.log(`Error ${error}`);
+            }
+        });
+    }
+}
+
