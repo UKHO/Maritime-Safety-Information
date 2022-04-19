266a8ad827cf07c5e7057351e7aaa79d0d606aeb
Vipin Mahajan <vipin14904@mastek.com>
2022-04-19 14:22:00 +0530

    AB#45787 AB#46067 added eventids

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Logging/EventIds.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Logging/EventIds.cs
@@ -3,5 +3,17 @@ namespace UKHO.MaritimeSafetyInformation.Common.Logging
 {
     public enum EventIds
     {
+        /// <summary>
+        /// 910001 - Event hub for MSI is healthy.
+        /// </summary>
+        EventHubLoggingIsHealthy = 910001,
+        /// <summary>
+        /// 910002 - Event hub for MSI is unhealthy.
+        /// </summary>
+        EventHubLoggingIsUnhealthy = 910002,
+        /// <summary>
+        /// 910003 -  Event data for MSI event hub health check.
+        /// </summary>
+        EventHubLoggingEventDataForHealthCheck = 910003,
     }
 }
