a54703df9431c6d59a4182769e2fb24062fb6186
Avinash15515 <avinash.patil@mastek.com>
2022-04-22 13:15:58 +0530

    AB# Unit Test for NMDataService

--- Tests/UKHO.MaritimeSafetyInformation.Web.UnitTests/Services/NMDataServiceTest.cs
+++ Tests/UKHO.MaritimeSafetyInformation.Web.UnitTests/Services/NMDataServiceTest.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using FakeItEasy;
+using NUnit.Framework;
+using UKHO.MaritimeSafetyInformation.Web.Services.Interfaces;
+using UKHO.MaritimeSafetyInformation.Web.Configuration;
+using Microsoft.Extensions.Configuration;
+using UKHO.MaritimeSafetyInformation.Web.Services;
 
 namespace UKHO.MaritimeSafetyInformation.Web.UnitTests.Services
 {
-    internal class NMDataServiceTest
+
+    public class NMDataServiceTest
     {
+        private IHttpClientFactory _fakehttpClientFactory;
+        private IFileShareService _fakefileShareService;
+        private IConfiguration _fakeconfiguration;
+        private FileShareServiceConfiguration _fakefileShareServiceConfig;
+        
+        private NMDataService _fakeNMDataService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakehttpClientFactory = A.Fake<IHttpClientFactory>();
+            _fakefileShareService = A.Fake<IFileShareService>();
+            _fakeconfiguration = A.Fake<IConfiguration>();
+            _fakefileShareServiceConfig = A.Fake<FileShareServiceConfiguration>();
+
+            _fakeNMDataService = new NMDataService(_fakefileShareService,_fakehttpClientFactory,_fakeconfiguration);
+        }
+
+
     }
 }
