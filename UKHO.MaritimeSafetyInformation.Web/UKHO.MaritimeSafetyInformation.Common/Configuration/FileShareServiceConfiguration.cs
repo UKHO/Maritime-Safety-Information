80c774ed16ce399eadf2ffbb6aaff8f88259bc74
Mohammed Khan <mohamme15315@mastek.com>
2022-04-26 18:04:30 +0530

    AB# - Added PageStart & Start variable in appsetting and configuration

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Configuration/FileShareServiceConfiguration.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Configuration/FileShareServiceConfiguration.cs
@@ -5,5 +5,7 @@
         public string BaseUrl { get; set; }
         public string BusinessUnit { get; set; }
         public string ProductType { get; set; }
+        public int PageSize { get; set; }
+        public int Start { get; set; }
     }
 }
