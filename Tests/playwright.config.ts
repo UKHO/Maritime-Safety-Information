9c553bb3171fe83d73753254a82f7a2ab1174cf4
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-04-28 12:40:40 +0530

    AB 45730

--- Tests/playwright.config.ts
+++ Tests/playwright.config.ts
@@ -4,17 +4,15 @@ import { devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   testDir: './AutoTests',
-<<<<<<< HEAD
+
     reporter: [ ['junit', { outputFile: 'a.xml' }] ],
 
  //  testDir: './tests/Accessebility Test',
  
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
-=======
-    timeout: 30 * 1000,
-  reporter: [['junit', { outputFile: 'junit.xml' }]],
->>>>>>> ec53106fcc3ace770ab9e57d3a4a2159d238b7ee
+   
+
   expect: {
     timeout: 10000
   },
@@ -22,12 +20,6 @@ const config: PlaywrightTestConfig = {
 
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-<<<<<<< HEAD
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
- // reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-=======
->>>>>>> ec53106fcc3ace770ab9e57d3a4a2159d238b7ee
   use: {
     actionTimeout: 0,
     trace: 'on-first-retry',
