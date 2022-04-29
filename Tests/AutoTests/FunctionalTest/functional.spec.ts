39186dcb91560d00335641b545820bb03f32e75d
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-04-29 10:42:42 +0530

    45730 UI test foundation functional spec AB#45730

--- Tests/AutoTests/FunctionalTest/functional.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional.spec.ts
@@ -1,34 +1,13 @@
 import { test, expect, chromium, Page, Browser, BrowserContext } from '@playwright/test';
 import * as app from "../../Configuration/appConfig.json";
-import home from '../../pageObject/home.page';
 
 test.describe("Goto maritime-safety-information Home Page", ()=> {
-  let browser: Browser;
-  let context: BrowserContext;
-  let page: Page;
-  let homepage:home;
-
-  test.beforeAll(async () => {
-    browser = await chromium.launch();
-  });
-
-  test.afterAll(async () => {
-     await browser.close();
-  });
   
-  test.afterEach(async () => {
-    await page.close();
-    await context.close();
-  });
-
-  test.beforeEach(async () => {
-    context = await browser.newContext();
-    page = await context.newPage();
+  test.beforeEach(async ({page}) => {
     await page.goto(app.url);  
-    const header = new home(page);
   });
 
- test('Should Goto Homepage Page',async()=>{   
-    await expect(page.url().toString()).toEqual(app.url);
-    } )
+  test('Should Goto Homepage Page',async({page})=>{   
+     await expect(page.url().toString()).toEqual(app.url);
+   } )   
 });
