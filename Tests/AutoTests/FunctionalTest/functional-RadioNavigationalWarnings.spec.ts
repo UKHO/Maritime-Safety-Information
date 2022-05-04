a980530f55090fe75f5ba136cf1f5ed105fba438
Kajal Kundu <kajalk@mastek.com>
2022-05-04 19:03:01 +0100

    modified functional tests and added accessibility tests AB#46799 AB#46773

--- Tests/AutoTests/FunctionalTest/functional-RadioNavigationalWarnings.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional-RadioNavigationalWarnings.spec.ts
@@ -1,35 +1,26 @@
-import { test, expect, chromium, Page, Browser, BrowserContext } from '@playwright/test';
+import { test } from '@playwright/test';
 import * as app from "../../Configuration/appConfig.json";
 import RadioNavigationalWarnings from '../../pageObject/RadioNavigationalWarnings.page';
 
 
-
-test.describe("create the rnw record", ()=> {
-  let rnw:any;
+test.describe("Create new radio navigational warnings record", ()=> {
+  let radioNavigationalWarnings:RadioNavigationalWarnings;
 
   test.beforeEach(async ({page}) => {
     await page.goto(app.url); 
-    const rnw = new RadioNavigationalWarnings(page);
-    rnw.creaternwrecord();
+    radioNavigationalWarnings = new RadioNavigationalWarnings(page);
+    radioNavigationalWarnings.clickCreateRadioNavigationalWarningsRecord();
   });
 
-   test('Withvaliddetails',async({page})=>{   
-    await rnw.warningtypedropdown('NavArea');
-    await rnw.refrencetext('refrence');
-    await rnw.datetimegroup('05042022');
-    await rnw.descriptiontext('testdata');
-    await rnw.contenttext('testdata');
-    await rnw.dialog('Record created successfully')
-    await rnw.createbuttonclick();
+   test('With valid input details',async()=>{   
+    await radioNavigationalWarnings.fillFormWithValidDetails("testdata");  
+    await radioNavigationalWarnings.clickCreateButton(); 
+    await radioNavigationalWarnings.getDialogText('Record created successfully')    
   } )
 
-  test('withValidDetailexceptContent',async({page})=>{   
-    await rnw.warningtypedropdown('NavArea');
-    await rnw.refrencetext('refrence');
-    await rnw.datetimegroup('05042022');
-    await rnw.descriptiontext('testdata');
-    await rnw.contenttext('testdata');
-    await rnw.dialog('Failed to create record.')
-    await rnw.createbuttonclick();
+  test('With invalid input as content text',async()=>{   
+    await radioNavigationalWarnings.fillFormWithValidDetails("testdata1");  
+    await radioNavigationalWarnings.clickCreateButton();
+    await radioNavigationalWarnings.getDialogText('Failed to create record.')    
  })  
 });
--- Tests/pageObject/RadioNavigationalWarnings.page.ts
+++ Tests/pageObject/RadioNavigationalWarnings.page.ts
@@ -1,69 +1,52 @@
-
 import { expect } from '@playwright/test';
 import type { Locator, Page } from 'playwright';
-
-
 
 export default class RadioNavigationalWarnings
 {
     private page:Page;
-    readonly refrence:Locator;
+    readonly reference:Locator;
     readonly datetime:Locator;
     readonly description:Locator;
-    readonly createnewrecord:Locator;    
+    readonly createNewRecord:Locator;    
     readonly content:Locator;
     readonly create:Locator;
-    readonly warning:string;
+    readonly warning:Locator;
     constructor(page:Page)
     {
         this.page = page; 
-        this.refrence = this.page.locator('#Reference');
+        this.reference = this.page.locator('#Reference');
         this.datetime= this.page.locator('#DateTimeGroup');
         this.description =  this.page.locator('#Summary');
-        this.createnewrecord = this.page.locator('text=create');
+        this.createNewRecord = this.page.locator('text=create');
         this.content=this.page.locator('#Content');
-       this.create= this.page.locator('body > main > div > div.row > div > form > div:nth-child(7) > input')
-       this.warning = '#WarningType';
+        this.create= this.page.locator('body > main > div > div.row > div > form > div:nth-child(7) > input')
+        this.warning = this.page.locator('#WarningType');
     }
 
-    public async creaternwrecord()
+    public async clickCreateRadioNavigationalWarningsRecord()
     {
-        this.createnewrecord.click();
-    }
-
-    public async warningtypedropdown(text:string)
-    {
-        this.page.selectOption(this.warning,text);   
-        
-    }
-
-    public async refrencetext(refercomment:string)
-    {
-       this.refrence.fill(refercomment);
-    }
-
-    public async datetimegroup(text:string)
-    {
-     this.datetime.fill(text)   
-    }
-    public async descriptiontext(text:string)
-    {
-        this.description.fill(text)
-    }
-    public async contenttext(text:string)
-    {
-       this.content.fill(text)
-    }
-    public async createbuttonclick()
+        this.createNewRecord.click();
+    }  
+   
+    public async clickCreateButton()
     {
        this.create.click();       
     }
 
-     public async dialog(text:string)
+    public async getDialogText(text:string)
     {
         this.page.on('dialog',(dialog)=>{
         expect(dialog.message).toEqual(text);
         dialog.accept();
-      })
-}
+      })     
+    }
+    
+    public async fillFormWithValidDetails(content:string)
+    {
+        this.warning.selectOption("NavArea");
+        this.reference.fill("reference");
+        this.datetime.fill("05042022");
+        this.description.fill("testdata");
+        this.content.fill(content);
+    }
 }
