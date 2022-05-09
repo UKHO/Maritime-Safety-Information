37c9c9363bec3e370a6cac4cd2661e0f4b2c6ad2
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-05-09 22:15:27 +0530

    45754 functional and accessibility test review comment AB#46773 AB#46799

--- Tests/pageObject/RadioNavigationalWarnings.page.ts
+++ Tests/pageObject/RadioNavigationalWarnings.page.ts
@@ -18,70 +18,54 @@ export default class RadioNavigationalWarnings
     readonly contentError:Locator;
     constructor(page:Page)
     {
-        this.page = page; 
-        this.reference = this.page.locator('#Reference');
-        this.datetime= this.page.locator('#DateTimeGroup');
-        this.description =  this.page.locator('#Summary');
-        this.createNewRecord = this.page.locator('text=Create New');
-        this.content=this.page.locator('#Content');
-        this.create= this.page.locator('#btnCreate')
-        this.warning = this.page.locator('#WarningType')
-        this.warningError=  this.page.locator("#WarningType-error");
-        this.referenceEror= this.page.locator("#Reference-error");
-        this.datetimeError= this.page.locator("#DateTimeGroup-error");
-        this.summaryError= this.page.locator("#Summary-error");
-        this.contentError=this.page.locator("#Content-error");
+       this.page = page; 
+       this.reference = this.page.locator('#Reference');
+       this.datetime= this.page.locator('#DateTimeGroup');
+       this.description =  this.page.locator('#Summary');
+       this.createNewRecord = this.page.locator('text=Create New');
+       this.content=this.page.locator('#Content');
+       this.create= this.page.locator('#btnCreate');
+       this.warning = this.page.locator('#WarningType');
+       this.warningError=  this.page.locator("#WarningType-error");
+       this.referenceEror= this.page.locator("#Reference-error");
+       this.datetimeError= this.page.locator("#DateTimeGroup-error");
+       this.summaryError= this.page.locator("#Summary-error");
+       this.contentError=this.page.locator("#Content-error");
     }
 
     public async clickCreateRadioNavigationalWarningsRecord()
     {
-      await this.createNewRecord.click();
+       await this.createNewRecord.click();
+       await expect(this.warning).toBeTruthy();
     }  
    
     public async clickCreateButton()
     {
-     await  this.create.click();       
+       await  this.create.click();
     }
 
     public async getDialogText(text:string)
     {
-       await this.page.on('dialog',async(dialog)=>{
-        expect(dialog.message()).toEqual(text);
-        dialog.accept();
-      })     
+       await this.page.on('dialog', async (dialog) => {
+       expect(dialog.message()).toEqual(text);
+       dialog.accept();
+        })     
     }   
 
-    public async getWarningErrorText(text:String)
+    public async getErrorMessage(locator:Locator,text:String)
     {
-        await expect((await this.warningError.textContent()).toString()).toEqual(text);   
+        expect((await locator.textContent()).toString()).toEqual(text);   
     }
-    public async getRefrenceErrorText(text:String)
-    {
-        await expect((await this.referenceEror.textContent()).toString()).toEqual(text);   
-    }
-
-    public async getDateErrorText(text:String)
-    {
-         await expect((await this.datetimeError.textContent()).toString()).toEqual(text);   
-    }
-    public async getDescriptionErrorText(text:String)
-    {
-        await expect((await this.summaryError.textContent()).toString()).toEqual(text);   
-    }
-    public async getConTextErrorText(text:String)
-    {
-        await expect((await this.contentError.textContent()).toString()).toEqual(text);   
-    }
-
     
-    public async fillFormWithValidDetails(content:string)
+    
+    public async fillFormWithValidDetails(warningType:string,content:string)
     {
-        await   this.warning.selectOption("1");
-        await  this.reference.fill("reference");
+        await this.warning.selectOption(warningType);
+        await this.reference.fill("reference");
         await this.page.waitForTimeout(3000);
-        await  this.datetime.type("05072022 05533");
-        await this.page.keyboard.press("ArrowDown")
-        await  this.description.fill("testdata");
-        await  this.content.fill(content);
+        await this.datetime.type("05072022 05533");
+        await this.page.keyboard.press("ArrowDown");
+        await this.description.fill("testdata");
+        await this.content.fill(content);
     }
 }
