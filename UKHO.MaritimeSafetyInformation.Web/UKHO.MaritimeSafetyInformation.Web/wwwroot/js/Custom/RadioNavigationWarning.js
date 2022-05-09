77fcd0f3a7e136b28d9b4025006200060ad02e37
Ashish Shelar <ashish15251@mastek.com>
2022-05-09 13:41:16 +0530

    AB#45754 Defects fix for datetime validation.

---
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/wwwroot/js/Custom/RadioNavigationWarning.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    $("#btnCreate").click(function () {
+        
+        var dt = $("#dateTimeGroup").val();
+
+        var d = new Date(dt);
+        var firstDate = new Date("1975-01-01");
+        var secondDate = new Date("2099-01-01");
+
+        if (d >= firstDate && d <= secondDate) {
+            //alert("Success");
+        }
+        else {
+            alert("Please provide date/time range between 01/01/1975 to 01/01/2099.");
+            return false;
+        }
+    });
+
+});
