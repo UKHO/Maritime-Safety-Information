3476edcd86bb72fd311a538a1ec5931c4f44990f
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-05-10 12:28:17 +0530

    45736 ui test automation AB#46754 AB#46805

--- Tests/AutoTests/FunctionalTest/functional-noticetomarine.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional-noticetomarine.spec.ts
@@ -6,11 +6,11 @@ test.describe("Maritime Safety Information Notice to Marine Page Functional Test
  
   let notice:any;
 
-  test.beforeEach(async ({page}) => {
-       await page.goto(app.url);  
-       notice = new noticeToMarine(page);
-       await notice.clickToNoticemarine();
-    });  
+     test.beforeEach(async ({page}) => {
+         await page.goto(app.url);  
+         notice = new noticeToMarine(page);
+         await notice.clickToNoticemarine();
+      });  
       test('Does the Yearly and Weekly Drop Down is enabled',async ({page}) => {
          const yearEnable = await notice.checkEnabledYearDropDown();
          expect(yearEnable).toBeTruthy();
@@ -19,10 +19,10 @@ test.describe("Maritime Safety Information Notice to Marine Page Functional Test
        })
   
       test('Does the Table Data For Yearly and Weekly Drop Down Include Table Data,File Name and File Size',async ({page}) => {
-        const tableData = await notice.getRecordCountTableNoticeToMarine("2022","16");
-        expect(tableData).toBeGreaterThan(0);   
-        expect(await notice.getFileNameText()).toEqual('File Name');
-        expect(await notice.getFileSizeText()).toEqual('File Size');  
-        await notice.getTableData();
+         const tableData = await notice.getRecordCountTableNoticeToMarine("2022","16");
+         expect(tableData).toBeGreaterThan(0);   
+         expect(await notice.getFileNameText()).toEqual('File Name');
+         expect(await notice.getFileSizeText()).toEqual('File Size');  
+         await notice.getTableData();
       })  
     })
