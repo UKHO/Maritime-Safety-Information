add93da1a5f1b18387f58167a90819490b825e1b
Ganpat Gawde <Ganpat14802@Mastek.Com>
2022-05-13 17:12:43 +0530

    45748 minor ui change AB#47508 AB#45748

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Models/RadioNavigationalWarning/RadioNavigationalWarningsData.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Models/RadioNavigationalWarning/RadioNavigationalWarningsData.cs
@@ -1,9 +1,12 @@
-﻿namespace UKHO.MaritimeSafetyInformation.Common.Models.RadioNavigationalWarning
+﻿using System.ComponentModel;
+
+namespace UKHO.MaritimeSafetyInformation.Common.Models.RadioNavigationalWarning
 {
     public class RadioNavigationalWarningsData
     {
         public string Reference { get; set; }
 
+        [DisplayName("Date Time Group")]
         public DateTime DateTimeGroup { get; set; }
 
         public string Description { get; set; }
