eb784e0c4bb9847688c91ccd391840eade14307b
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-05-16 17:45:06 +0530

    46539 ui test automation AB#46792 AB#46806

--- Tests/AutoTests/FunctionalTest/functional-RadioNavigationalWarnings.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional-RadioNavigationalWarnings.spec.ts
@@ -2,28 +2,30 @@ import { test, expect, chromium, Page, Browser, BrowserContext } from '@playwrig
 import * as app from "../../Configuration/appConfig.json";
 import RadioNavigationalWarningsList from '../../pageObject/RadioNavigationalWarningsAdminList.page';
 
-test.describe("Goto maritime-safety-information Home Page", ()=> {
-  let RadioNavigationalWarningsList:RadioNavigationalWarningsList;
-  test.beforeEach(async ({page}) => {
+test.describe("Goto maritime-safety-information Admin List Page", ()=> {
+   let radioNavigationalWarningsList:RadioNavigationalWarningsList;
+   
+   test.beforeEach(async ({page}) => {
    await page.goto(app.adminurl);  
-  });
+   radioNavigationalWarningsList = new RadioNavigationalWarningsList(page);
+   });
 
   test('Does the Warning Type,Year Drop Down is enabled and Page Header Text,Create Record Text is Displayed',async ({page}) => {
-   const WarningTypeEnable = await RadioNavigationalWarningsList.checkEnabledWarningTypeDropDown();
-   expect(WarningTypeEnable).toBeTruthy();
-   const yearEnable = await RadioNavigationalWarningsList.checkEnabledYearDropDown();
+   let warningTypeEnable = await radioNavigationalWarningsList.checkEnabledWarningTypeDropDown();
+   expect(warningTypeEnable).toBeTruthy();
+   const yearEnable = await radioNavigationalWarningsList.checkEnabledYearDropDown();
    expect(yearEnable).toBeTruthy();
-   const createRecordList= await RadioNavigationalWarningsList.checkCreateNewrecordText();
-   expect(createRecordList).toBeTruthy();
-   const pageHeader = await RadioNavigationalWarningsList.checkPageHeaderText();
-   expect(pageHeader).toContain("Radio Navigational Warnings List");
-})
+   let createRecordList= await radioNavigationalWarningsList.checkCreateNewrecordText();
+   expect(createRecordList).toEqual("Create New Warning");
+   let pageHeader = await radioNavigationalWarningsList.checkPageHeaderText();
+   expect(pageHeader).toEqual("Radio Navigational Warnings List");
+   })
 
   test('Does the Table data is displayed with Pagination',async ({page}) => {
-   RadioNavigationalWarningsList.getTableList();
-   RadioNavigationalWarningsList.pagination(RadioNavigationalWarningsList.btnFirst)
-   RadioNavigationalWarningsList.pagination(RadioNavigationalWarningsList.btnLast)
-   RadioNavigationalWarningsList.pagination(RadioNavigationalWarningsList.btnNext)
-   RadioNavigationalWarningsList.pagination(RadioNavigationalWarningsList.btnPrevious)
+   await radioNavigationalWarningsList.getTableList();
+   await radioNavigationalWarningsList.pagination(radioNavigationalWarningsList.btnFirst);
+   await radioNavigationalWarningsList.pagination(radioNavigationalWarningsList.btnLast);
+   await radioNavigationalWarningsList.pagination(radioNavigationalWarningsList.btnNext);
+   await radioNavigationalWarningsList.pagination(radioNavigationalWarningsList.btnPrevious);
 })
 });
--- Tests/pageObject/RadioNavigationalWarningsAdminList.page.ts
+++ Tests/pageObject/RadioNavigationalWarningsAdminList.page.ts
@@ -3,118 +3,93 @@ import { expect } from '@playwright/test';
 import type { Locator, Page } from 'playwright';
 
 export default class RadioNavigationalWarningsList
-{
-    private page:Page;
-    readonly warningType:Locator;
-    readonly year:Locator;
-    readonly filter:Locator;
-    readonly createNewRecordText:Locator;
-    readonly btnFirst:Locator;
-    readonly btnPrevious:Locator;
-    readonly btnNext:Locator;
-    readonly btnLast:Locator;
-    constructor(page:Page)
-    {
+   {
+       private page:Page;
+       readonly warningType:Locator;
+       readonly year:Locator;
+       readonly filter:Locator;
+       readonly createNewRecordText:Locator;
+       readonly btnFirst:Locator;
+       readonly btnPrevious:Locator;
+       readonly btnNext:Locator;
+       readonly btnLast:Locator;
+       readonly pageHeaderText:Locator;
+       constructor(page:Page)
+       {
         this.page = page; 
         this.warningType = this.page.locator("#WarningType");
         this.year= this.page.locator("#Year");
         this.filter = this.page.locator("#BtnFilter");
-        this.createNewRecordText= this.page.locator("#BtnCreate");
+        this.createNewRecordText= this.page.locator("text=Create New Warning");
+        this.pageHeaderText=this.page.locator("text=Radio Navigational Warnings List");
         this.btnFirst= this.page.locator("#BtnFirst");
         this.btnPrevious =this.page.locator("#BtnPrevious");
         this.btnNext= this.page.locator("#BtnNext");
         this.btnLast =this.page.locator("#BtnLast");
-    }
-
-    public async goToCreateRadioNavigationalWarningsRecordList()
-    {
-      this.page.locator().click();
-    } 
-    public async checkEnabledWarningTypeDropDown()
-    {
+       }
+    
+       public async checkEnabledWarningTypeDropDown()
+       {
         return this.warningType.isEnabled();
-    }
-    public async checkEnabledYearDropDown()
-    {
-        return this.year.isEnabled();
-    }
+       }
+       public async checkEnabledYearDropDown()
+       {
+       return  this.year.isEnabled();
+       }
 
-    public async checkCreateNewrecordText()
-    {
-        return this.createNewRecordText.innerText().toString();
-    }
+       public async checkCreateNewrecordText()
+       {
+       return this.createNewRecordText.textContent();
+       }
 
-    public async checkPageHeaderText()
-    {
-      return this.page.locator("").innerText().toString();
-    }
+       public async checkPageHeaderText()
+       {
+       return await this.pageHeaderText.textContent();
+       }
 
-    public async checkEnabledFilterButton()
-    {
-      return this.page.locator("").isEnabled();
-    }
+       public async checkEnabledFilterButton()
+       {
+       return await this.filter.isEnabled();
+       }
     
-    public async getTableList()
-    {
-    const warningTypeCount = (await this.page.$$("#WarningType option")).length;
-    const yearCount = (await this.page.$$("#Year option")).length;
+       public async getTableList()
+       {
+
+       const warningTypeCount = (await this.page.$$("#WarningType option")).length;
+       const yearCount = (await this.page.$$("#Year option")).length;
+
+       for(var warningType=1;warningType<=warningTypeCount-1;warningType++)
+       {
+       await this.warningType.selectOption({index:warningType});
     
-     for(var warningType=0;warningType<warningTypeCount;warningType++)
-    {
-    await this.warningType.selectOption({index:warningType});
-     
-    for(var year=0;year<yearCount;year++)
-    {
-      await this.year.selectOption({index:year});
-      await this.filter.click();
+       for(var year=1;year<=yearCount-1;year++)
+       {
+       await this.year.selectOption({index:year});
+       await this.filter.click();
       
-      const fileSizeData = await this.page.$$("");
+       const fileSizeData = await this.page.$$("table > tbody > tr >td:nth-child(2)");
+       const edit = await this.page.$$("table > tbody > tr >td:nth-child(9)");
       
-      for await (const table of fileSizeData)
-      {
-          console.log((await table.innerText()).toString());
-      }
-    }
-   
-}
+       expect(fileSizeData.length).toBeLessThanOrEqual(20);
+       for await (const table of fileSizeData)
+       {
+       const warning =await table.innerText();
+       if(warning=="UK Coastal" || warning=="NAVAREA 1")
+       {
+        expect(warning).toBeTruthy();
+       }
+       else throw new Error("No Warning Type");
+       }
+       for await (const tableEdit of edit)
+       {
+       expect((await tableEdit.innerText()).toString()).toEqual("Edit");
+       }      
+       }
+       }
+       }
+       public async pagination(locator:Locator)
+       {  
+       expect(locator.isEnabled).toBeTruthy();
+       } 
 
-const editOption = await (await this.page.$$(""));
-
-for (const editOptionList of editOption)
-{
-  expect(editOptionList.innerText().toString()).toContain("Edit");
-}
-
-for(var warningType=1;warningType<warningTypeCount;warningType++)
-    {
-        await this.warningType.selectOption({index:warningType});
-        await this.filter.click();
-        const fileSizeData = await this.page.$$("tbody tr td");
-        expect((await fileSizeData.length)).toBeLessThanOrEqual(20);
-      
-      for await (const table of fileSizeData)
-      {
-          console.log((await table.innerText()).toString());
-     
-      }
-      
-    }
-    for(var year=1;year<yearCount;year++)
-    {
-      await this.year.selectOption({index:year});
-      await this.filter.click();
-      const fileSizeData = await this.page.$$("");
-      expect(await fileSizeData.length).toEqual(20);
-      for await (const table of fileSizeData)
-      {
-          console.log((await table.innerText()).toString());
-      }
-    }
-  }
-  
-  public async pagination(locator:Locator)
-  {   
-      expect(locator.isEnabled()).toBeTruthy();
-  }
- 
 }
