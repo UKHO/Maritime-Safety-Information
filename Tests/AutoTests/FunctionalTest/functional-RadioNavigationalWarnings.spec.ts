3db39f1453f5ea7bf3efe693570b92b3fa6b91d9
Vipin Mahajan <vipin14904@mastek.com>
2022-05-17 15:32:12 +0530

    AB#46539 AB#46792 Test case varaible name change

--- Tests/AutoTests/FunctionalTest/functional-RadioNavigationalWarnings.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional-RadioNavigationalWarnings.spec.ts
@@ -6,7 +6,7 @@ test.describe("Goto maritime-safety-information Admin List Page", ()=> {
    let radioNavigationalWarningsList:RadioNavigationalWarningsList;
    
    test.beforeEach(async ({page}) => {
-   await page.goto(app.adminurl);  
+       await page.goto(app.RnwAdminUrl);
    radioNavigationalWarningsList = new RadioNavigationalWarningsList(page);
    });
 
