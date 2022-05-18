4ab0cab8bd2d3b3b9fb9ec1986bc372201252476
Vipin Mahajan <vipin14904@mastek.com>
2022-05-18 15:55:58 +0530

    AB#46539 AB#47412 added  unit test case

--- Tests/UKHO.MaritimeSafetyInformation.Web.UnitTests/Services/RnwRepositoryTest.cs
+++ Tests/UKHO.MaritimeSafetyInformation.Web.UnitTests/Services/RnwRepositoryTest.cs
@@ -19,33 +19,49 @@ namespace UKHO.MaritimeSafetyInformation.Web.UnitTests.Services
         private RadioNavigationalWarningsContext _fakeContext;
         private ILogger<RnwRepository> _fakeLogger;
 
-        [SetUp]
-        public void SetUp()
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
         {
             DbContextOptionsBuilder<RadioNavigationalWarningsContext> builder = new DbContextOptionsBuilder<RadioNavigationalWarningsContext>()
-                                                                                .UseInMemoryDatabase("msi-ut-db");
-
+                                                                    .UseInMemoryDatabase("msi-ut-db");
             _fakeContext = new RadioNavigationalWarningsContext(builder.Options);
-            _fakeLogger = A.Fake<ILogger<RnwRepository>>();
-
-            _rnwRepository = new RnwRepository(_fakeContext, _fakeLogger);
-
-            _fakeContext.RadioNavigationalWarnings.RemoveRange(_fakeContext.RadioNavigationalWarnings);
-            _fakeContext.WarningType.RemoveRange(_fakeContext.WarningType);
 
             _fakeContext.RadioNavigationalWarnings.AddRange(GetFakeRadioNavigationalWarningList());
             _fakeContext.WarningType.AddRange(GetFakeWarningTypeList());
             _fakeContext.SaveChanges();
         }
 
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeLogger = A.Fake<ILogger<RnwRepository>>();
+            _rnwRepository = new RnwRepository(_fakeContext, _fakeLogger);
+        }
+
         [Test]
         public async Task WhenCallGetRadioNavigationWarnings_ThenReturnListAsync()
         {
-            List<RadioNavigationalWarningsAdminList> result = await _rnwRepository.GetRadioNavigationWarningsAdminList();            
+            List<RadioNavigationalWarningsAdminList> result = await _rnwRepository.GetRadioNavigationWarningsAdminList();
             Assert.IsTrue(result.Count == 4);
             Assert.AreEqual(4, result[0].Id);
             Assert.AreEqual("011200 UTC Jan 22", result[0].DateTimeGroupRnwFormat);
             Assert.AreEqual("UK Coastal", result[0].WarningTypeName);
+        }
+
+        [Test]
+        public async Task WhenCallGetRadioNavigationWarnings_ThenIsDeletedShouldDisplayYesAndNoRespectively()
+        {
+            List<RadioNavigationalWarningsAdminList> result = await _rnwRepository.GetRadioNavigationWarningsAdminList();
+            Assert.IsTrue(result[0].IsDeleted == "No");
+            Assert.IsTrue(result[2].IsDeleted == "Yes");
+        }
+
+        [Test]
+        public async Task WhenCallGetRadioNavigationWarningWithContentLenthGreaterThan300Char_ThenWrapTheContent()
+        {
+            List<RadioNavigationalWarningsAdminList> result = await _rnwRepository.GetRadioNavigationWarningsAdminList();
+            Assert.IsTrue(result[2].Content.Length <= 303);
+            Assert.IsTrue(result[2].Content.Contains("..."));
         }
 
         [Test]
@@ -63,9 +79,17 @@ namespace UKHO.MaritimeSafetyInformation.Web.UnitTests.Services
         {
             List<string> result = await _rnwRepository.GetYears();
             Assert.IsTrue(result.Count == 3);
-            Assert.AreEqual("2022", result[0]);
+            Assert.AreEqual("2020", result[0]);
             Assert.AreEqual("2021", result[1]);
-            Assert.AreEqual("2020", result[2]);
+            Assert.AreEqual("2022", result[2]);
+        }
+
+        [OneTimeTearDown]
+        public void GlobalTearDown()
+        {
+            _fakeContext.RadioNavigationalWarnings.RemoveRange(_fakeContext.RadioNavigationalWarnings);
+            _fakeContext.WarningType.RemoveRange(_fakeContext.WarningType);
+            _fakeContext.SaveChanges();
         }
 
         #region PrivateMethod
@@ -78,7 +102,14 @@ namespace UKHO.MaritimeSafetyInformation.Web.UnitTests.Services
                 Reference = "RnwAdminListReferance",
                 DateTimeGroup = new DateTime(2020, 1, 1),
                 Summary = "RnwAdminListSummary",
-                Content = "RnwAdminListContent"
+                Content = "1. NAVAREA I WARNINGS IN FORCE AT 221000 UTC APR 22:     2021 SERIES: 031.  2022 SERIES: 033, 041, 043, 044."
+                 + "NOTES:  A. Texts of NAVAREA I Warnings issued each week are published in weekly editions of the ADMIRALTY Notices to"
+                 + "Mariners bulletin (ANMB).  B. NAVAREA I Warnings less than 42 days old (033/22 onward) are promulgated via Enhanced"
+                 + "Group Call (EGC) and/or relevant NAVTEX transmitters.  C. The complete texts of all in-force NAVAREA I warnings,"
+                 + "including those which are no longer being broadcast, are reprinted in Section III of ANMB in weeks 1, 13, 26 and 39"
+                 + "and are also available from the UKHO website at: www.admiralty.co.uk/RNW.  Alternatively, these may be requested by"
+                 + "e-mail from NAVAREA I Co-ordinator at: navwarnings@ukho.gov.uk    2. Cancel NAVAREA I 042/22.",
+                IsDeleted = true
             });
 
             radioNavigationalWarningList.Add(new RadioNavigationalWarnings()
