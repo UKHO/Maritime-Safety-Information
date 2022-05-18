f7a3d262bb2d04f4dab155b7a822169cf51261d8
Tejasi Chavan <tejasi15072@mastek.com>
2022-05-18 11:56:55 +0530

    AB#46539-add db connection string in keyvault

--- Deployment/src/variables.tf
+++ Deployment/src/variables.tf
@@ -8,18 +8,35 @@ variable "resource_group_name" {
   default = "msi"
 }
 
+variable "rnw_db_server" {
+
+}
+
+variable "rnw_db_name" {
+
+}
+
+variable "rnw_db_app_user" {
+
+}
+
+variable "rnw_db_app_pass" {
+
+}
+
 locals {
-  env_name           = lower(terraform.workspace)
-  service_name       = "msi"
-  web_app_name       = "${local.service_name}-${local.env_name}-webapp"
-  key_vault_name     = "${local.service_name}-ukho-${local.env_name}-kv"
+  env_name                    = lower(terraform.workspace)
+  service_name                = "msi"
+  web_app_name                = "${local.service_name}-${local.env_name}-webapp"
+  key_vault_name              = "${local.service_name}-ukho-${local.env_name}-kv"
+  rnw_db_connection_string    = "Server=${var.rnw_db_server};Initial Catalog=${var.rnw_db_name};Persist Security Info=False;User ID=${var.rnw_db_app_user};Password=${var.rnw_db_app_pass};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication="Active Directory Password";"
   tags = {
-    SERVICE          = "Maritime Safety Information"
-    ENVIRONMENT      = local.env_name
-    SERVICE_OWNER    = "UKHO"
-    RESPONSIBLE_TEAM = "Mastek"
-    CALLOUT_TEAM     = "On-Call_N/A"
-    COST_CENTRE      = "A.008.02"
+    SERVICE                   = "Maritime Safety Information"
+    ENVIRONMENT               = local.env_name
+    SERVICE_OWNER             = "UKHO"
+    RESPONSIBLE_TEAM          = "Mastek"
+    CALLOUT_TEAM              = "On-Call_N/A"
+    COST_CENTRE               = "A.008.02"
   }
 }
 
