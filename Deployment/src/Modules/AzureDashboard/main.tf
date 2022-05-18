ecc68a0ffaac9b0a07302b15892e4c104f1cc5dc
Barrie Cooper <barrie.cooper@ukho.gov.uk>
2022-05-18 18:42:57 +0100

    Correct vars

--- Deployment/src/Modules/AzureDashboard/main.tf
+++ Deployment/src/Modules/AzureDashboard/main.tf
@@ -7,7 +7,9 @@ resource "azurerm_dashboard" "azure-dashboard" {
   location            = var.resource_group.location
   tags                = var.tags
   dashboard_properties = templatefile("${path.module}/dashboard.tpl", {
-    subscription_id = data.azurerm_subscription.current.subscription_id,
-    environment = var.environment
+    subscription_id     = data.azurerm_subscription.current.subscription_id,
+    environment         = var.environment,
+    resource_group_name = var.resource_group.name,
+    web_app_name        = var.web_app_name
   })
 }
