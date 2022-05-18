efa47952d781434056b8c5e17f0fdf1a95353ef0
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-05-18 11:04:18 +0530

    45736 46346 ui test automation AB#46754 AB#46805

---
+++ Tests/AutoTests/AccessebilityTest/accessebility-noticetomarine.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect, chromium, Page, Browser, BrowserContext } from '@playwright/test';
+
+import * as app from "../../Configuration/appConfig.json";
+import noticetomarine from '../../pageObject/noticetomarine.page';
+import { injectAxe, getViolations, checkA11y, Options} from 'axe-playwright'
+
+test.describe("MSI Notice to Marine Page Accessibility Test", () => {
+    let notice:any;
+    const defaultCheckA11yOptions: Options = {
+        axeOptions: {
+            runOnly: {
+                type: 'tag',
+                values: ['wcag2aa'],
+            },
+            reporter: 'v2'
+        },
+        detailedReport: true,
+        detailedReportOptions: { html: true }
+    };
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto(app.url);
+        notice = new noticetomarine(page);
+        await notice.clickToNoticemarine();
+    });
+
+    test('Notice to Marine page should be accessible', async ({ page }) => {
+
+        await injectAxe(page);
+        await checkA11y(page, undefined, defaultCheckA11yOptions);
+    })
+});
