6277f3b6135879b967042f33e5b72402d0d4c599
Vipin Mahajan <vipin14904@mastek.com>
2022-05-19 12:23:47 +0530

    AB#46539 Sql Connection string changes Added

--- Deployment/src/variables.tf
+++ Deployment/src/variables.tf
@@ -8,11 +8,22 @@ variable "resource_group_name" {
   default = "msi"
 }
 
+variable "rnw_db_server" {
+}
+variable "rnw_db_name" {
+}
+variable "rnw_db_app_sqluser" {
+}
+variable "rnw_db_app_sqlpass" {
+}
+
 locals {
   env_name                    = lower(terraform.workspace)
   service_name                = "msi"
   web_app_name                = "${local.service_name}-${local.env_name}-webapp"
   key_vault_name              = "${local.service_name}-ukho-${local.env_name}-kv"
+   rnw_db_connection_string = "Server=${var.rnw_db_server};Initial Catalog=${var.rnw_db_name};Persist Security Info=False;User ID=${var.rnw_db_app_sqluser};Password=${var.rnw_db_app_sqlpass};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+
   tags = {
     SERVICE                   = "Maritime Safety Information"
     ENVIRONMENT               = local.env_name
--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/Startup.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/Startup.cs
@@ -29,8 +29,7 @@ namespace UKHO.MaritimeSafetyInformation.Web
         {
             //Enables Application Insights telemetry.
             services.AddApplicationInsightsTelemetry();
-            services.AddDbContext<RadioNavigationalWarningsContext>(options => options.UseSqlServer(configuration.GetConnectionString("RadioNavigationalWarningsContext")));
-
+    
             services.AddLogging(loggingBuilder =>
             {
                 loggingBuilder.AddConfiguration(configuration.GetSection("Logging"));
@@ -39,9 +38,13 @@ namespace UKHO.MaritimeSafetyInformation.Web
                 loggingBuilder.AddAzureWebAppDiagnostics();
             });
             services.Configure<EventHubLoggingConfiguration>(configuration.GetSection("EventHubLoggingConfiguration"));
-            services.Configure<RadioNavigationalWarningConfiguration>(configuration.GetSection("RadioNavigationalWarningConfiguration"));
-            
+            services.Configure<RadioNavigationalWarningConfiguration>(configuration.GetSection("RadioNavigationalWarningConfiguration"));     
             services.Configure<FileShareServiceConfiguration>(configuration.GetSection("FileShareService"));
+            services.Configure<RadioNavigationalWarningsContextConfiguration>(configuration.GetSection("RadioNavigationalWarningsContext"));
+
+            var msiDBConfiguration = new RadioNavigationalWarningsContextConfiguration();
+            configuration.Bind("RadioNavigationalWarningsContext", msiDBConfiguration);
+            services.AddDbContext<RadioNavigationalWarningsContext>(options => options.UseSqlServer(msiDBConfiguration.ConnectionString));
 
             services.AddScoped<IEventHubLoggingHealthClient, EventHubLoggingHealthClient>();
             services.AddScoped<INMDataService, NMDataService>();
