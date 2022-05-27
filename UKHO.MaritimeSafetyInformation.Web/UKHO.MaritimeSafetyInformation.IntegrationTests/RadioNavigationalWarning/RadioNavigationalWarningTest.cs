9fdf5f00a4e111eb21daa2c3bb12e641c11d2fab
Vipin Mahajan <vipin14904@mastek.com>
2022-05-27 15:41:48 +0530

    AB#47343 Use  exact type

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.IntegrationTests/RadioNavigationalWarning/RadioNavigationalWarningTest.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.IntegrationTests/RadioNavigationalWarning/RadioNavigationalWarningTest.cs
@@ -42,7 +42,7 @@ namespace UKHO.MaritimeSafetyInformation.IntegrationTests.RadioNavigationalWarni
         {
             IActionResult result = await _controller.Index();
             List<RadioNavigationalWarningsData> warningsData = (List<RadioNavigationalWarningsData>)((ViewResult)result).Model;
-            var lastModifiedDateTime = ((ViewResult)result).ViewData["LastModifiedDateTime"].ToString();
+            string lastModifiedDateTime = ((ViewResult)result).ViewData["LastModifiedDateTime"].ToString();
             Assert.AreEqual("150215 UTC Aug 19", lastModifiedDateTime);
             Assert.AreEqual(6, warningsData.Count);
         }
