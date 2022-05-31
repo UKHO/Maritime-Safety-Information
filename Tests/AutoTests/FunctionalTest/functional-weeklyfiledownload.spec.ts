8f57d9cad03ad681bf3197270e39bb867ce75bc0
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-05-31 14:56:09 +0530

    47195 review comment AB#48394

--- Tests/AutoTests/FunctionalTest/functional-weeklyfiledownload.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional-weeklyfiledownload.spec.ts
@@ -3,7 +3,7 @@ import { test, expect, chromium, Page, Browser, BrowserContext } from '@playwrig
 import * as app from "../../Configuration/appConfig.json";
 import noticeToMarinerWeekDownload from '../../pageObject/noticeToMarine-weekfiledownload.page';
 
-test.describe("Goto maritime-safety-information Notice To Marine Page", () => {
+test.describe("Goto maritime-safety-information Notice To Mariner Page", () => {
   let noticeFileDownload: noticeToMarinerWeekDownload;
   test.beforeEach(async ({ page }) => {
     await page.goto(app.url);
@@ -25,8 +25,6 @@ test.describe("Goto maritime-safety-information Notice To Marine Page", () => {
   test('Should Goto Notice to Mariner Page for Daily download File', async ({ page, context }) => {
     await noticeFileDownload.goToNoticeToMariner();
     await noticeFileDownload.goToDailyFile();
-    const firstFileDownloadName = await noticeFileDownload.getFirstDailyFileName();
-    const fileDownloadName=await noticeFileDownload.checkDailyFileDownload();
-    expect(fileDownloadName).toContain(`fileName=${firstFileDownloadName}`);
+    await noticeFileDownload.checkDailyFileDownload();
   })
 });
--- Tests/pageObject/noticeToMarine-weekfiledownload.page.ts
+++ Tests/pageObject/noticeToMarine-weekfiledownload.page.ts
@@ -1,68 +1,57 @@
 
+import { expect } from '@playwright/test';
 import type { Locator, Page } from 'playwright';
 
 
 
-export default class noticeToMarinerWeekDownload
-{
-    private page:Page;
-    readonly noticeToMarine:Locator;
-    readonly year:Locator;
-    readonly week:Locator;
-    readonly daily:Locator;
-    constructor(page:Page)
-    {
-        this.page = page; 
-        this.noticeToMarine = this.page.locator('a:has-text("Notices to Mariners")');
-        this.year = this.page.locator('#ddlYears');
-        this.week =this.page.locator('#ddlWeeks');
-        this.daily = this.page.locator('a[role="tab"]:has-text("Daily")');
+export default class noticeToMarinerWeekDownload {
+  private page: Page;
+  readonly noticeToMarine: Locator;
+  readonly year: Locator;
+  readonly week: Locator;
+  readonly daily: Locator;
+  readonly download: Locator;
+  readonly fileName: Locator;
+  constructor(page: Page) {
+    this.page = page;
+    this.noticeToMarine = this.page.locator('a:has-text("Notices to Mariners")');
+    this.year = this.page.locator('#ddlYears');
+    this.week = this.page.locator('#ddlWeeks');
+    this.daily = this.page.locator('a[role="tab"]:has-text("Daily")');
+    this.download = this.page.locator("[id^='download'] > a");
+    this.fileName = this.page.locator("[id^='filename']");
+  }
+
+  public async goToNoticeToMariner() {
+    await this.noticeToMarine.click();
+  }
+
+  public async goToDailyFile() {
+    await this.daily.click();
+  }
+  public async checkFileDownload() {
+    await this.year.selectOption('2022');
+    await this.week.selectOption('17');
+    await this.page.waitForSelector("[id^='filename']");
+    const result = this.page.$$eval("[id^='filename']", (options: any[]) => { return options.map(option => option.textContent.trim()) });
+    return result;
+  }
+
+  public async checkDailyFileDownload() {
+    await this.page.waitForSelector("[id^='filename']");
+    const dailyfileName = await this.fileName.first().evaluate((name) => name.textContent);
+
+    if ((await dailyfileName).length > 0) {
+      const dailyDownloadPageUrl = await (await this.download.first().getAttribute('href')).trim().split("&");
+      const downloadurl = dailyDownloadPageUrl[1].replace(/%20/g, " ");
+      expect(downloadurl).toContain(`fileName=${dailyfileName}`);
     }
-
-    public async goToNoticeToMariner()
-    {
-      await this.noticeToMarine.click();
-    } 
-
-    public async goToDailyFile()
-    {
-      await this.daily.click();
+    else {
+      throw new Error("No download File Found")
     }
-    public async checkFileDownload()
-    {
-     await this.year.selectOption('2022');
-     await this.week.selectOption('17');
-     await this.page.waitForSelector("[id^='filename']");
-     const result = this.page.$$eval("[id^='filename']", (options : any[]) => {return options.map( option=>option.textContent.trim())}); 
-     return result; 
-     }
+  }
+}
 
-     public async checkDailyFileDownload()
-     {
-      await this.page.waitForSelector("[id^='filename']");
-      const dailyfileName = this.page.$$eval("[id^='filename']", (options : any[]) => {return options.map( option=>option.textContent.trim())}); 
-        
-      if ((await dailyfileName).length > 0) {
-        const fileName = dailyfileName[0];
-        var dailyPageUrl = await (await (await this.page.$("[id^='download'] > a")).getAttribute('href')).trim().split("&");
-        const newDailyPageUrl = dailyPageUrl[1].replace(/%20/g, " ");
-        return newDailyPageUrl;
-      }
-      else {
-        throw new Error("No download File Found")
-      }
 
-     }
 
-     public async getFirstDailyFileName()
-     {
-      await this.page.waitForSelector("[id^='filename']");
-      const dailyfileName = await this.page.$$eval("[id^='filename']", (options : any[]) => {return options.map( option=>option.textContent.trim())}); 
-      const fileName = dailyfileName[0];
-      return fileName;
-     }
-   } 
-     
-   
-    
-   
+
