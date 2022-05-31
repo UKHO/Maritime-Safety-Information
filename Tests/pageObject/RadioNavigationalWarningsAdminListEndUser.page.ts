691b28f98ef6ca6970dca822b5efee99bba64c35
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-05-31 10:32:38 +0530

    47343 review comment AB#48402

--- Tests/pageObject/RadioNavigationalWarningsAdminListEndUser.page.ts
+++ Tests/pageObject/RadioNavigationalWarningsAdminListEndUser.page.ts
@@ -3,108 +3,102 @@ import { expect } from '@playwright/test';
 import type { Locator, Page } from 'playwright';
 import { DateTime } from 'luxon';
 
-export default class RadioNavigationalWarningsListEndUser
-{
-    private page:Page;
-    readonly radioNavigationalWarningsPage:Locator;
-    readonly radioNavigationalWarningsEndUser:Locator;
-    readonly radioWarningEndUser:Locator;
-    readonly aboutEndUser:Locator;
-    readonly allWarningEndUser:Locator;
-    readonly navAreaEndUser:Locator;
-    readonly ukCostalEnduser:Locator;
-    readonly tableHeader:Locator;
-    readonly tableHeaderText =['Reference','Date Time Group','Description','Select all','Select'];
-    constructor(page:Page)
-    {
-         this.page = page; 
-         this.radioNavigationalWarningsPage = this.page.locator('a:has-text("Radio Navigational Warnings")')
-         this.radioNavigationalWarningsEndUser = this.page.locator('#headingLevelOne')
-         this.radioWarningEndUser = this.page.locator('text=Radio Warnings')
-         this.aboutEndUser = this.page.locator('text=About')
-         this.allWarningEndUser = this.page.locator('#allwarnings-tab')
-         this.navAreaEndUser = this.page.locator('#NAVAREA1-tab')
-         this.ukCostalEnduser = this.page.locator('#ukcoastal-tab')
-         
-    }
 
-    public async goToRadioWarning()
-    {
-      await this.radioNavigationalWarningsPage.click();
-    } 
-    
-    public async checkText(locator:Locator)
-    {
-     return await locator.innerText();
-    }
+export default class RadioNavigationalWarningsListEndUser {
+  private page: Page;
+  readonly radioNavigationalWarningsPage: Locator;
+  readonly radioNavigationalWarningsEndUser: Locator;
+  readonly radioWarningEndUser: Locator;
+  readonly aboutEndUser: Locator;
+  readonly allWarningEndUser: Locator;
+  readonly navAreaEndUser: Locator;
+  readonly ukCostalEnduser: Locator;
+  readonly tableHeader: Locator;
+  readonly tableHeaderText = ['Reference', 'Date Time Group', 'Description', 'Select all', 'Select'];
+  constructor(page: Page) {
+    this.page = page;
+    this.radioNavigationalWarningsPage = this.page.locator('a:has-text("Radio Navigational Warnings")')
+    this.radioNavigationalWarningsEndUser = this.page.locator('#headingLevelOne')
+    this.radioWarningEndUser = this.page.locator('text=Radio Warnings')
+    this.aboutEndUser = this.page.locator('text=About')
+    this.allWarningEndUser = this.page.locator('#allwarnings-tab')
+    this.navAreaEndUser = this.page.locator('#NAVAREA1-tab')
+    this.ukCostalEnduser = this.page.locator('#ukcoastal-tab')
 
-    public async verifyTableDateColumnData()
-    {
-      const resultYear= await this.page.$$eval('[id^="DateTimeGroupRnwFormat"]' , (matches: any[]) => { return matches.map(option => option.textContent.trim().slice(-2)) });
-  
-      //fail if there are no matching selections
-      expect(resultYear.length).toBeGreaterThan(0);
+  }
 
-      //Verify Dates are descending order   
-      const resultdate= await this.page.$$eval('[id^="DateTimeGroupRnwFormat"]' , (matches: any[]) => { return matches.map(option => option.textContent.trim().slice(6)) });
-      const sortedDesc = resultdate.sort((objA, objB) => objB.date - objA.date , );
-      expect(sortedDesc).toBeTruthy();  
-      
-    }
-  
-    public async verifyTableContainsViewDetailsLink()
-    {
-      const resultLinks= await this.page.$$eval('[id^="Viewdetails"] > button' , (matches: any[]) => { return matches.map(option => option.textContent.trim()) });
-     
-      for(let i=0;i<resultLinks.length;i++)
-      {
-        expect(resultLinks[i].trim()).toEqual("details");
-      }
-    }
+  public async goToRadioWarning() {
+    await this.radioNavigationalWarningsPage.click();
+  }
 
-    public async verifyTableHeader()
-    {
-      let tableColsHeader = await this.page.$$eval('.table>thead>tr>th', (options: any[]) => { return options.map(option => option.textContent.trim()) });
-      tableColsHeader=tableColsHeader.filter(Boolean);
-      var match = (this.tableHeaderText.length == tableColsHeader.length) && this.tableHeaderText.every(function (element, index) {
+  public async checkText(locator: Locator) {
+    return await locator.innerText();
+  }
+
+  public async verifyTableDateColumnData() {
+    const resultYear = await this.page.$$eval('[id^="DateTimeGroupRnwFormat"]', (matches: any[]) => { return matches.map(option => option.textContent.trim().slice(-2)) });
+
+    //fail if there are no matching selections
+    expect(resultYear.length).toBeGreaterThan(0);
+
+    //Verify Dates are descending order   
+    const resultdate = await this.page.$$eval('[id^="DateTimeGroupRnwFormat"]', (matches: any[]) => { return matches.map(option => option.textContent.trim().slice(6)) });
+    const sortedDesc = resultdate.sort((objA, objB) => objB.date - objA.date,);
+    expect(sortedDesc).toBeTruthy();
+
+  }
+
+  public async verifyTableContainsViewDetailsLink() {
+    const resultLinks = await this.page.$$eval('[id^="Viewdetails"] > button', (matches: any[]) => { return matches.map(option => option.textContent.trim()) });
+
+    for (let i = 0; i < resultLinks.length; i++) {
+      expect(resultLinks[i].trim()).toEqual("details");
+    }
+  }
+
+  public async verifyTableHeader() {
+    let tableColsHeader = await this.page.$$eval('.table>thead>tr>th', (options: any[]) => { return options.map(option => option.textContent.trim()) });
+    tableColsHeader = tableColsHeader.filter(Boolean);
+    var match = (this.tableHeaderText.length == tableColsHeader.length) && this.tableHeaderText.every(function (element, index) {
       return element === tableColsHeader[index];
-      });
-      expect(match).toBeTruthy();
-    }
-  
-    public async verifyTableViewDetailsUrl()
-  {
-      const viewDetails= await this.page.$('[id^="Viewdetails"] >> text=details');
-      const beforeRefrence= await (await this.page.$('[id^="Reference"]')).innerText();
-      const beforeDatetime = await (await this.page.$('[id^="DateTimeGroupRnwFormat"]')).innerText();
-      const beforeViewDetails = await (await this.page.$('[id^="Viewdetails"] >> text=details')).getAttribute("aria-expanded");
-      expect(beforeViewDetails).toEqual("false");
-      await viewDetails.click({force:true});
-      const newDetails = await (await this.page.$('[id^="Viewdetails"] >> text=details')).getAttribute("aria-expanded");
-      expect(newDetails).toBeTruthy();
-      const afterRefrence = await (await this.page.$('[id^="Details_Reference"]')).innerText();
-      const afterDateTime = await (await this.page.$('[id^="Details_DateTimeGroupRnwFormat"]')).innerText();
-      expect(beforeRefrence).toEqual(afterRefrence);
-      expect(beforeDatetime).toEqual(afterDateTime);   
-}
+    });
+    expect(match).toBeTruthy();
+  }
 
-    public async verifyImportantBlock()
-    {
-      var newRnw=this.page.locator("#rnwInfo > p").innerText();
-      const rnw = (await newRnw).split(":");
+  public async verifyTableViewDetailsUrl() {
+    const viewDetails = await this.page.$('[id^="Viewdetails"] >> text=details');
+    const beforeRefrence = await (await this.page.$('[id^="Reference"]')).innerText();
+    const beforeDatetime = await (await this.page.$('[id^="DateTimeGroupRnwFormat"]')).innerText();
+    const beforeViewDetails = await (await this.page.$('[id^="Viewdetails"] >> text=details')).getAttribute("aria-expanded");
+    expect(beforeViewDetails).toEqual("false");
+    await viewDetails.click({ force: true });
+    const newDetails = await (await this.page.$('[id^="Viewdetails"] >> text=details')).getAttribute("aria-expanded");
+    expect(newDetails).toBeTruthy();
+    const afterRefrence = await (await this.page.$('[id^="Details_Reference"]')).innerText();
+    const afterDateTime = await (await this.page.$('[id^="Details_DateTimeGroupRnwFormat"]')).innerText();
+    expect(beforeRefrence).toEqual(afterRefrence);
+    expect(beforeDatetime).toEqual(afterDateTime);
+  }
 
-      var newrnw = rnw[0];
-      expect(newrnw).toContain("NAVAREA 1 and UK Coastal");
-      var todayDateTime=DateTime.now().toFormat('ddhhmm MMM yy').trim();
-      const rnwDateTime = rnw[1].replace('UTC',"").trim();
-      
-      if(rnwDateTime<todayDateTime)
-      {
-      expect(rnwDateTime).toBeTruthy();
+  public async verifyImportantBlock() {
+    const rnwHeader = this.page.locator("#rnwInfo > p").innerText();
+    const rnwHeaderText = (await rnwHeader).split(":");
+
+    const rnwMessageText = rnwHeaderText[0];
+    expect(rnwMessageText).toContain("NAVAREA 1 and UK Coastal");
+
+    const currentDateTime = new Date().getTime();
+    const rnwDateTime = rnwHeaderText[1].replace('UTC', "").trim();
+
+    const rnwModifiedDateTime = DateTime.fromFormat(rnwDateTime, "ddhhmm  MMM yy").toString();
+    const lastModifiedDateTime = new Date(rnwModifiedDateTime);
+
+    const compareDate = (currentDateTime, lastModifiedDateTime) => {
+      if (lastModifiedDateTime < currentDateTime) {
+        return true;
       }
-      else
-      {
-      expect(rnwDateTime).toBeFalsy();
-      } 
+      else { return false; }
     }
+    expect(compareDate).toBeTruthy();
+  }
 }  
