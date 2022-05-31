f274a71678bec11ec18752c274340eec8609b47a
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-05-31 16:21:22 +0530

    46538 review comment AB#47551

--- Tests/AutoTests/FunctionalTest/functional-RadioNavigationalWarningsCreateRNW.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional-RadioNavigationalWarningsCreateRNW.spec.ts
@@ -42,37 +42,6 @@ test.describe("Create new radio navigational warnings record", ()=> {
     await radioNavigationalWarnings.createRNW(); 
     await radioNavigationalWarnings.getDialogText('Record created successfully!')    
   })
-
-  test('Update With summary text as blank',async({page})=>{       
-    await radioNavigationalWarnings.getEditUrl();
-    await radioNavigationalWarnings.clearInput(radioNavigationalWarnings.description);
-    await radioNavigationalWarnings.clearInput(radioNavigationalWarnings.reference);
-    await radioNavigationalWarnings.clearInput(radioNavigationalWarnings.content);
-    await radioNavigationalWarnings.editRNW();
-    await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.summaryError,'The Description field is required.'); 
-    await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.referenceEror,"The Reference field is required.");
-    await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.contentError,'The Text field is required.');
-   
-  }) 
- 
-   test('Update With valid input details with UK Coastal',async({page})=>{   
-    await radioNavigationalWarnings.searchListWithfilter('UK Coastal');
-    await radioNavigationalWarnings.getEditUrl();
-    await radioNavigationalWarnings.isDelete();
-    await radioNavigationalWarnings.fillEditFormWithValidDetails("testdata");  
-    await radioNavigationalWarnings.editRNW(); 
-    await radioNavigationalWarnings.getDialogText('Record updated successfully!')    
-  })
-
-   test('Update With valid input details with NAVAREA',async({page})=>{  
-    await radioNavigationalWarnings.searchListWithfilter('NAVAREA 1');
-    await radioNavigationalWarnings.getEditUrl();
-    await radioNavigationalWarnings.isDelete();
-    await radioNavigationalWarnings.fillEditFormWithValidDetails("testdata");  
-    await radioNavigationalWarnings.editRNW(); 
-    await radioNavigationalWarnings.getDialogText('Record updated successfully!')    
-  })
-
 });
   
 
