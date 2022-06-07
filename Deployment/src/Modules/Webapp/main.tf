2718d006d1479348ac074b50ca6e33bcee9dd06b
Tejasi Chavan <tejasi15072@mastek.com>
2022-06-07 12:47:08 +0530

    Add admin webapp build and deploymnet steps AB#48572 AB#49022

--- Deployment/src/Modules/Webapp/main.tf
+++ Deployment/src/Modules/Webapp/main.tf
@@ -36,3 +36,34 @@ resource "azurerm_app_service_virtual_network_swift_connection" "webapp_vnet_int
   app_service_id = azurerm_windows_web_app.webapp_service.id
   subnet_id      = var.subnet_id
 }
+
+#Admin Webapp
+resource "azurerm_windows_web_app" "admin_webapp_service" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  service_plan_id     = azurerm_service_plan.app_service_plan.id
+  tags                = var.tags
+
+  site_config {
+     application_stack {    
+     current_stack = "dotnet"
+     dotnet_version = "v6.0"
+    }
+    always_on  = true
+    ftps_state = "Disabled"
+
+   }
+  app_settings = var.app_settings
+
+  identity {
+    type = "SystemAssigned"
+    }
+
+  https_only = true
+  }
+
+resource "azurerm_app_service_virtual_network_swift_connection" "admin_webapp_vnet_integration" {
+  app_service_id = azurerm_windows_web_app.admin_webapp_service.id
+  subnet_id      = var.subnet_id
+}
