fa7c1c854dd5f3e7d57607f4f225df295d6aa4d3
Vijendra Kurmi <vijendr13545@Mastek.com>
2022-06-07 21:57:10 +0530

    Added unit tests AB#45725 AB#49067

---
+++ Tests/UKHO.MaritimeSafetyInformation.Web.UnitTests/Controllers/MsiAccountControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using FakeItEasy;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using UKHO.MaritimeSafetyInformation.Web.Controllers;
+
+namespace UKHO.MaritimeSafetyInformation.Web.UnitTests.Controllers
+{
+    [TestFixture]
+    public class MsiAccountControllerTests
+    {
+        private MsiAccountController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            var ctx = new DefaultHttpContext();
+            _controller = new MsiAccountController();
+            _controller.ControllerContext = new() { HttpContext = ctx };
+            _controller.Url = A.Fake<IUrlHelper>();
+        }
+
+        [Test]
+        public void SignOutAction_ReturnsSignOutResult()
+        {
+            IActionResult result = _controller.SignOut();
+
+            Assert.IsInstanceOf<SignOutResult>(result);
+        }
+
+        [Test]
+        public void SignOutAction_UsesRequiredAuthenticationScheme()
+        {
+            IActionResult result = _controller.SignOut();
+            
+            SignOutResult signOutResult = ((SignOutResult)result);
+
+            Assert.IsInstanceOf<SignOutResult>(result);
+            Assert.IsTrue(signOutResult.AuthenticationSchemes.Contains(OpenIdConnectDefaults.AuthenticationScheme));
+            Assert.IsTrue(signOutResult.AuthenticationSchemes.Contains(CookieAuthenticationDefaults.AuthenticationScheme));
+        }
+    }
+}
---
+++ Tests/UKHO.MaritimeSafetyInformation.Web.UnitTests/Services/UserServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using NUnit.Framework;
+using UKHO.MaritimeSafetyInformation.Web.Services;
+using FakeItEasy;
+
+namespace UKHO.MaritimeSafetyInformation.Web.UnitTests.Services
+{
+    [TestFixture]
+    public class UserServiceTests
+    {
+        [Test]
+        public void WhenUserIsUnautheticated_ThenIsDistributorReturnsFalse()
+        {
+            DefaultHttpContext httpContext = new DefaultHttpContext();
+
+            var mockHttpContextAccessor = A.Fake<HttpContextAccessor>();
+            mockHttpContextAccessor.HttpContext = httpContext;
+
+            UserService userService = new UserService(mockHttpContextAccessor);
+
+            Assert.AreEqual(false, userService.IsDistributorUser);
+        }
+
+        [Test]
+        public void WhenUserNotInRole_ThenIsDistributorReturnsFalse()
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, "Test User"),
+                    new Claim(ClaimTypes.Role, "Test Role 1")
+                }, "mock"));
+
+            DefaultHttpContext httpContext = new DefaultHttpContext()
+            {
+                User = user
+            };
+
+            var mockHttpContextAccessor = A.Fake<HttpContextAccessor>();
+            mockHttpContextAccessor.HttpContext = httpContext;
+
+            UserService userService = new UserService(mockHttpContextAccessor);
+
+            Assert.AreEqual(false, userService.IsDistributorUser);
+        }
+
+        [Test]
+        public void WhenUserHasRole_ThenIsDistributorReturnsTrue()
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, "Test User"),
+                    new Claim(ClaimTypes.Role, UserService.DISTRIBUTOR_ROLE_NAME)
+                }, "mock"));
+
+            DefaultHttpContext httpContext = new DefaultHttpContext()
+            {
+                User = user
+            };
+
+            var mockHttpContextAccessor = A.Fake<HttpContextAccessor>();
+            mockHttpContextAccessor.HttpContext = httpContext;
+
+            UserService userService = new UserService(mockHttpContextAccessor);
+
+            Assert.AreEqual(true, userService.IsDistributorUser);
+        }
+    }
+}
