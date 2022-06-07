1feb9af1ec03992e6f0c266a0adc9cb5a25eb0a5
Ganpat Gawde <Ganpat14802@Mastek.Com>
2022-06-07 18:51:36 +0530

    AB#45753 added minor changes  in js file AB#48361 AB#49015

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/wwwroot/js/Custom/RadioNavigationalWarnings-admin.js
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/wwwroot/js/Custom/RadioNavigationalWarnings-admin.js
@@ -75,3 +75,27 @@ function terms_changed() {
         }
     }
 }
+
+function printDiv() {
+    //Get the HTML of div
+    var divElements = document.getElementById("printDiv").innerHTML;
+    //Get the HTML of whole page
+    //var oldPage = document.body.innerHTML;
+
+    ////Reset the page's HTML with div's HTML only
+    //document.body.innerHTML =
+    //    "<html><head><title></title></head><body>" +
+    //    divElements + "</body>";
+
+    //Print Page
+    window.print();
+
+    ////Restore orignal HTML
+    //document.body.innerHTML = oldPage;
+
+
+}
+
+function display() {
+    window.print();
+}
