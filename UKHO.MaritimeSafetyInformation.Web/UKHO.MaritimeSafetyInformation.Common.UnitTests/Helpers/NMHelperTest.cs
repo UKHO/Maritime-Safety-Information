cfecc47a31907ac0cf67085254610266cff479ba
Shirin14926 <85286507+Shirin14926@users.noreply.github.com>
2022-06-10 16:15:13 +0530

    48897 NM duplicate files logic for weekly daily Files (#40)
    * 48897 code changes, unit test case and Integrtaion test for Weekly duplicate files AB#48897
    
    * 48897 daily logic code AB#48897
    
    * 48897 daily duplicate code logic AB#48897
    
    * 48897 correct failing unit test case AB#48897
    
    * 48897 dailydownload integration test missing AB#48897
    
    * 48897 add trim logic for year/week in nmhelper AB#28897
    
    * 48897 refactor code logic AB#48897
    
    * 48897 refactor code AB#48897
    
    * 48897 code rework AB#48897
    
    * 48897 review comments fixed AB#48897

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common.UnitTests/Helpers/NMHelperTest.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common.UnitTests/Helpers/NMHelperTest.cs
@@ -15,7 +15,6 @@ namespace UKHO.MaritimeSafetyInformation.Common.UnitTests.Helpers
 {
     public class NMHelperTest
     {
-
         [Test]
         public void WhenNMHelperCallsListFilesResponse_ThenConversionIsCorrect()
         {
@@ -87,6 +86,54 @@ namespace UKHO.MaritimeSafetyInformation.Common.UnitTests.Helpers
         }
 
         [Test]
+        public void WhenNMHelperCallsListFilesResponseWithDuplicateData_ThenReturnLatestData()
+        {
+            BatchSearchResponse searchResult = SetSearchResultDuplicateDataForWeekly();
+
+            List<ShowFilesResponseModel> expected = new()
+            {
+                new ShowFilesResponseModel()
+                {
+                    BatchId = "1",
+                    Filename = "aaa.pdf",
+                    FileDescription = "aaa",
+                    FileExtension = ".pdf",
+                    FileSize = 1232,
+                    FileSizeinKB = "1 KB",
+                    MimeType = "PDF",
+                    Links = null
+                },
+                new ShowFilesResponseModel()
+                {
+                    BatchId = "1",
+                    Filename = "bbb.pdf",
+                    FileDescription = "bbb",
+                    FileExtension = ".pdf",
+                    FileSize = 1232,
+                    FileSizeinKB = "1 KB",
+                    MimeType = "PDF",
+                    Links = null
+                }
+            };
+
+            List<ShowFilesResponseModel> result = NMHelper.ListFilesResponse(searchResult);
+
+            Assert.Multiple(() =>
+            {
+                for (int i = 0; i < result.Count; i++)
+                {
+                    Assert.AreEqual(expected[i].BatchId, result[i].BatchId);
+                    Assert.AreEqual(expected[i].Filename, result[i].Filename);
+                    Assert.AreEqual(expected[i].FileDescription, result[i].FileDescription);
+                    Assert.AreEqual(expected[i].FileExtension, result[i].FileExtension);
+                    Assert.AreEqual(expected[i].FileSize, result[i].FileSize);
+                    Assert.AreEqual(expected[i].FileSizeinKB, result[i].FileSizeinKB);
+                    Assert.AreEqual(expected[i].MimeType, result[i].MimeType);
+                }
+            });
+        }
+
+        [Test]
         public void WhenGetDailyShowFilesResponseIsCalled_ThenConversionIsCorrect()
         {
             BatchSearchResponse searchResult = SetSearchResultForDaily();
@@ -95,7 +142,7 @@ namespace UKHO.MaritimeSafetyInformation.Common.UnitTests.Helpers
             {
                 new ShowDailyFilesResponseModel
                 {
-                    YearWeek = "2022 / 17",
+                    YearWeek = "2022/17",
                     WeekNumber = "17",
                     Year = "2022",
                     DailyFilesData = new()
@@ -128,7 +175,7 @@ namespace UKHO.MaritimeSafetyInformation.Common.UnitTests.Helpers
                 },
                 new ShowDailyFilesResponseModel
                 {
-                    YearWeek = "2022 / 18",
+                    YearWeek = "2022/18",
                     WeekNumber = "18",
                     Year = "2022",
                     DailyFilesData = new()
@@ -159,6 +206,79 @@ namespace UKHO.MaritimeSafetyInformation.Common.UnitTests.Helpers
                         }
                     }
                 }
+            };
+            expected = expected.OrderByDescending(x => x.Year).ThenByDescending(x => x.WeekNumber).ToList();
+
+            foreach (var item in expected)
+            {
+                item.DailyFilesData = item.DailyFilesData.OrderBy(x => Convert.ToDateTime(x.DataDate)).ToList();
+            }
+
+            List<ShowDailyFilesResponseModel> result = NMHelper.GetDailyShowFilesResponse(searchResult);
+            Assert.Multiple(() =>
+            {
+                for (int i = 0; i < result.Count; i++)
+                {
+                    Assert.AreEqual(expected[i].Year, result[i].Year);
+                    Assert.AreEqual(expected[i].YearWeek, result[i].YearWeek);
+                    Assert.AreEqual(expected[i].WeekNumber, result[i].WeekNumber);
+
+                    for (int j = 0; j < expected[i].DailyFilesData.Count; j++)
+                    {
+                        Assert.AreEqual(expected[i].DailyFilesData[j].BatchId, result[i].DailyFilesData[j].BatchId);
+                        Assert.AreEqual(expected[i].DailyFilesData[j].DataDate, result[i].DailyFilesData[j].DataDate);
+                        Assert.AreEqual(expected[i].DailyFilesData[j].Filename, result[i].DailyFilesData[j].Filename);
+                        Assert.AreEqual(expected[i].DailyFilesData[j].FileDescription, result[i].DailyFilesData[j].FileDescription);
+                        Assert.AreEqual(expected[i].DailyFilesData[j].FileExtension, result[i].DailyFilesData[j].FileExtension);
+                        Assert.AreEqual(expected[i].DailyFilesData[j].FileSizeInKB, result[i].DailyFilesData[j].FileSizeInKB);
+                        Assert.AreEqual(expected[i].DailyFilesData[j].MimeType, result[i].DailyFilesData[j].MimeType);
+                        Assert.AreEqual(expected[i].DailyFilesData[j].Links, result[i].DailyFilesData[j].Links);
+                        Assert.AreEqual(expected[i].DailyFilesData[j].AllFilesZipSize, result[i].DailyFilesData[j].AllFilesZipSize);
+                    }
+                }
+            });
+        }
+
+        [Test]
+        public void WhenGetDailyShowFilesResponseIsCalledWithDuplicateData_ThenLatestDistinctIsReturned()
+        {
+            BatchSearchResponse searchResult = SetSearchResultDuplicateDataForDaily();
+
+            List<ShowDailyFilesResponseModel> expected = new()
+            {
+                new ShowDailyFilesResponseModel
+                {
+                    YearWeek = "2022/07",
+                    WeekNumber = "07",
+                    Year = "2022",
+                    DailyFilesData = new()
+                    {
+                        new DailyFilesDataModel()
+                        {
+                            DataDate = "2022-02-15",
+                            BatchId = "8cd563e1-a8e4-4a7d-84bb-3f60fddec0fe",
+                            Filename = "Daily 15-02-22.zip",
+                            FileDescription = "Daily 15-02-22.zip",
+                            FileExtension = ".zip",
+                            FileSizeInKB = FileHelper.FormatSize(400040),
+                            MimeType = "application/gzip",
+                            Links = null,
+                            AllFilesZipSize = 400040
+                        },
+                        new DailyFilesDataModel()
+                        {
+                            DataDate = "2022-02-16",
+                            BatchId = "9cd969e6-a1e1-7a7d-44bb-6f60fddec9fe",
+                            Filename = "Daily 16-02-22.zip",
+                            FileDescription = "Daily 16-02-22.zip",
+                            FileExtension = ".zip",
+                            FileSizeInKB = FileHelper.FormatSize(300040),
+                            MimeType = "application/gzip",
+                            Links = null,
+                            AllFilesZipSize = 300040
+                        }
+                    }
+                }                
             };
             expected = expected.OrderByDescending(x => x.Year).ThenByDescending(x => x.WeekNumber).ToList();
 
@@ -276,6 +396,58 @@ namespace UKHO.MaritimeSafetyInformation.Common.UnitTests.Helpers
                     },
                     new BatchDetails() {
                         BatchId = "2",
+                        Files = new List<BatchDetailsFiles>() {
+                            new BatchDetailsFiles () {
+                                Filename = "ccc.pdf",
+                                FileSize=1232,
+                                MimeType = "PDF",
+                                Links = null
+                            },
+                            new BatchDetailsFiles () {
+                                Filename = "ddd.pdf",
+                                FileSize=1232,
+                                MimeType = "PDF",
+                                Links = null
+                            }
+                        }
+
+                    }
+                }
+            };
+
+            return searchResult;
+        }
+
+        private static BatchSearchResponse SetSearchResultDuplicateDataForWeekly()
+        {
+            BatchSearchResponse searchResult = new()
+            {
+                Count = 2,
+                Links = null,
+                Total = 0,
+                Entries = new List<BatchDetails>() {
+                    new BatchDetails() {
+                        BatchId = "1",
+                        BatchPublishedDate = DateTime.Now,
+                        Files = new List<BatchDetailsFiles>() {
+                            new BatchDetailsFiles () {
+                                Filename = "aaa.pdf",
+                                FileSize=1232,
+                                MimeType = "PDF",
+                                Links = null
+                            },
+                            new BatchDetailsFiles () {
+                                Filename = "bbb.pdf",
+                                FileSize=1232,
+                                MimeType = "PDF",
+                                Links = null
+                            }
+                        }
+
+                    },
+                    new BatchDetails() {
+                        BatchId = "2",
+                        BatchPublishedDate= DateTime.Now.AddMinutes(-10),
                         Files = new List<BatchDetailsFiles>() {
                             new BatchDetailsFiles () {
                                 Filename = "ccc.pdf",
@@ -469,6 +641,148 @@ namespace UKHO.MaritimeSafetyInformation.Common.UnitTests.Helpers
 
                         }
                     }
+            };
+
+            return searchResult;
+        }
+
+        private static BatchSearchResponse SetSearchResultDuplicateDataForDaily()
+        {
+            BatchSearchResponse searchResult = new()
+            {
+                Count = 1,
+                Links = null,
+                Total = 0,
+                Entries = new List<BatchDetails>() {
+                        new BatchDetails() {
+                            BatchId = "75920ffc-4820-47eb-be76-aaa3209eb3b6",
+                            AllFilesZipSize=299170,
+                            Attributes = new List<BatchDetailsAttributes>()
+                            {
+                                new BatchDetailsAttributes("Data Date","2022-02-15"),
+                                new BatchDetailsAttributes("Frequency","Daily"),
+                                new BatchDetailsAttributes("Product Type","Notices to Mariners"),
+                                new BatchDetailsAttributes("Week Number","07"),
+                                new BatchDetailsAttributes("Year","2022"),
+                                new BatchDetailsAttributes("Year / Week","2022 / 07"),
+
+                            },
+                            BusinessUnit = "TEST",
+                            BatchPublishedDate = DateTime.Now.AddMinutes(-10),
+                            ExpiryDate = DateTime.Now,
+                            Files = new List<BatchDetailsFiles>() {
+                                new BatchDetailsFiles () {
+                                    Filename = "aaa.pdf",
+                                    FileSize=1232,
+                                    MimeType = "PDF",
+                                    Links = null
+                                },
+                                new BatchDetailsFiles () {
+                                    Filename = "bbb.pdf",
+                                    FileSize=1232,
+                                    MimeType = "PDF",
+                                    Links = null
+                                }
+                            }
+
+                        },
+                        new BatchDetails() {
+                            BatchId = "1cd579e1-a1e2-4a7d-94bb-1f90fddec7fe",
+                            AllFilesZipSize=60040,
+                            Attributes = new List<BatchDetailsAttributes>()
+                            {
+                                new BatchDetailsAttributes("Data Date","2022-02-15"),
+                                new BatchDetailsAttributes("Frequency","Daily"),
+                                new BatchDetailsAttributes("Product Type","Notices to Mariners"),
+                                new BatchDetailsAttributes("Week Number","07"),
+                                new BatchDetailsAttributes("Year","2022"),
+                                new BatchDetailsAttributes("Year / Week","2022 / 07"),
+
+                            },
+                            BusinessUnit = "TEST",
+                            BatchPublishedDate = DateTime.Now.AddMinutes(-20),
+                            ExpiryDate = DateTime.Now,
+                            Files = new List<BatchDetailsFiles>() {
+                                new BatchDetailsFiles () {
+                                    Filename = "ccc.pdf",
+                                    FileSize=1232,
+                                    MimeType = "PDF",
+                                    Links = null
+                                },
+                                new BatchDetailsFiles () {
+                                    Filename = "ddd.pdf",
+                                    FileSize=1232,
+                                    MimeType = "PDF",
+                                    Links = null
+                                }
+                            }
+                        },
+
+                        new BatchDetails() {
+                            BatchId = "8cd563e1-a8e4-4a7d-84bb-3f60fddec0fe",
+                            AllFilesZipSize=400040,
+                            Attributes = new List<BatchDetailsAttributes>()
+                            {
+                                new BatchDetailsAttributes("Data Date","2022-02-15"),
+                                new BatchDetailsAttributes("Frequency","Daily"),
+                                new BatchDetailsAttributes("Product Type","Notices to Mariners"),
+                                new BatchDetailsAttributes("Week Number","07"),
+                                new BatchDetailsAttributes("Year","2022"),
+                                new BatchDetailsAttributes("Year / Week","2022 / 07"),
+
+                            },
+                            BusinessUnit = "TEST",
+                            BatchPublishedDate = DateTime.Now,
+                            ExpiryDate = DateTime.Now,
+                            Files = new List<BatchDetailsFiles>() {
+                                new BatchDetailsFiles () {
+                                    Filename = "aaa.pdf",
+                                    FileSize=1232,
+                                    MimeType = "PDF",
+                                    Links = null
+                                },
+                                new BatchDetailsFiles () {
+                                    Filename = "bbb.pdf",
+                                    FileSize=1232,
+                                    MimeType = "PDF",
+                                    Links = null
+                                }
+                            }
+
+                        },
+                        new BatchDetails() {
+                            BatchId = "9cd969e6-a1e1-7a7d-44bb-6f60fddec9fe",
+                            AllFilesZipSize=300040,
+                            Attributes = new List<BatchDetailsAttributes>()
+                            {
+                                new BatchDetailsAttributes("Data Date","2022-02-16"),
+                                new BatchDetailsAttributes("Frequency","Daily"),
+                                new BatchDetailsAttributes("Product Type","Notices to Mariners"),
+                                new BatchDetailsAttributes("Week Number","07"),
+                                new BatchDetailsAttributes("Year","2022"),
+                                new BatchDetailsAttributes("Year / Week","2022 / 07"),
+
+                            },
+                            BusinessUnit = "TEST",
+                            BatchPublishedDate = DateTime.Now,
+                            ExpiryDate = DateTime.Now,
+                            Files = new List<BatchDetailsFiles>() {
+                                new BatchDetailsFiles () {
+                                    Filename = "ccc.pdf",
+                                    FileSize=1232,
+                                    MimeType = "PDF",
+                                    Links = null
+                                },
+                                new BatchDetailsFiles () {
+                                    Filename = "ddd.pdf",
+                                    FileSize=1232,
+                                    MimeType = "PDF",
+                                    Links = null
+                                }
+                            }
+
+                        }
+                }
             };
 
             return searchResult;
