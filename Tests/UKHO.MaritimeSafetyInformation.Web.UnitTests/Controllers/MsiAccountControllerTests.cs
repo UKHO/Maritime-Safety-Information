a17c9772ae46c98bbe7a5f84896b827ad0fc644f
Shirin14926 <Shirin.talawdekar@mastek.com>
2022-06-11 16:15:26 +0530

    45725 remove vulnerabilites and add unit test case AB#45725

--- Tests/UKHO.MaritimeSafetyInformation.Web.UnitTests/Controllers/MsiAccountControllerTests.cs
+++ Tests/UKHO.MaritimeSafetyInformation.Web.UnitTests/Controllers/MsiAccountControllerTests.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
 using NUnit.Framework;
 using UKHO.MaritimeSafetyInformation.Web.Controllers;
 
@@ -17,29 +19,42 @@ namespace UKHO.MaritimeSafetyInformation.Web.UnitTests.Controllers
         public void Setup()
         {
             var ctx = new DefaultHttpContext();
-            _controller = new MsiAccountController();
-            _controller.ControllerContext = new() { HttpContext = ctx };
-            _controller.Url = A.Fake<IUrlHelper>();
+            _controller = new MsiAccountController
+            {
+                ControllerContext = new() { HttpContext = ctx }
+            };
         }
 
         [Test]
-        public void SignOutAction_ReturnsSignOutResult()
+        public void WhenSignOutActionCalled_ThenReturnsSignOutResult()
         {
+            _controller.Url = A.Fake<IUrlHelper>();
             IActionResult result = _controller.SignOut();
 
             Assert.IsInstanceOf<SignOutResult>(result);
         }
 
         [Test]
-        public void SignOutAction_UsesRequiredAuthenticationScheme()
+        public void WhenSignOutActionCalled_ThenUsesRequiredAuthenticationScheme()
         {
+            _controller.Url = A.Fake<IUrlHelper>();
             IActionResult result = _controller.SignOut();
-            
+
             SignOutResult signOutResult = ((SignOutResult)result);
 
             Assert.IsInstanceOf<SignOutResult>(result);
             Assert.IsTrue(signOutResult.AuthenticationSchemes.Contains(OpenIdConnectDefaults.AuthenticationScheme));
             Assert.IsTrue(signOutResult.AuthenticationSchemes.Contains(CookieAuthenticationDefaults.AuthenticationScheme));
+        }
+
+        [Test]
+        public void WhenSignedOutActionCalled_ThenSignout()
+        {  
+            _controller.Url = new UrlHelper(new ActionContext { RouteData = new RouteData() });
+            IActionResult result = _controller.SignedOut();
+
+            Assert.IsInstanceOf<RedirectResult>(result);
+            Assert.IsTrue(result != null);
         }
     }
 }
