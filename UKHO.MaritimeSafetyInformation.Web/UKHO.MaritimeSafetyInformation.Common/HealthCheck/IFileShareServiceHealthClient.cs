f76ea330ef969ade9b51b7a2310b338eb767c998
vipin14904 <85299812+vipin14904@users.noreply.github.com>
2022-06-13 19:20:03 +0530

    47305/47306 Health check RNW database and FSS (#42)
    * AB#47305 health check for rnw databse
    
    * AB#47305 AB#48997 AB#48996 Added unit test cases
    
    * AB#47306 AB#49004 AB#49005 Dev code and unit testing
    
    * AB#47305  AB#48997  AB#47306 AB#49005 added unit test cases
    
    * AB#47305 Exception added
    
    * AB#47305 added coverlet.msbuild
    
    * AB#47305-47306 Review comments fixed AB#47305-47306
    
    Co-authored-by: Ashish Shelar <ashish15251@mastek.com>

---
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/HealthCheck/IFileShareServiceHealthClient.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace UKHO.MaritimeSafetyInformation.Common.HealthCheck
+{
+    public interface IFileShareServiceHealthClient
+    {
+        Task<HealthCheckResult> CheckHealthAsync(CancellationToken cancellationToken);
+    }
+}
