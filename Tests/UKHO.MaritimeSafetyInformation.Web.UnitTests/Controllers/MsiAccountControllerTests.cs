eeaee3ebfcdd3d661ba4a14cd2b82b8d55df4bae
Shirin14926 <Shirin.talawdekar@mastek.com>
2022-06-13 17:28:24 +0530

    45725 fix unit tests AB#45725

--- Tests/UKHO.MaritimeSafetyInformation.Web.UnitTests/Controllers/MsiAccountControllerTests.cs
+++ Tests/UKHO.MaritimeSafetyInformation.Web.UnitTests/Controllers/MsiAccountControllerTests.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
-using Microsoft.AspNetCore.Routing;
 using NUnit.Framework;
 using UKHO.MaritimeSafetyInformation.Web.Controllers;
 
@@ -14,47 +13,54 @@ namespace UKHO.MaritimeSafetyInformation.Web.UnitTests.Controllers
     public class MsiAccountControllerTests
     {
         private MsiAccountController _controller;
+        private IUrlHelper _fakeUrlHelper;
 
         [SetUp]
         public void Setup()
         {
             var ctx = new DefaultHttpContext();
+            _fakeUrlHelper = A.Fake<IUrlHelper>();
+
             _controller = new MsiAccountController
             {
-                ControllerContext = new() { HttpContext = ctx }
+                ControllerContext = new() { HttpContext = ctx },
+                Url = _fakeUrlHelper
             };
         }
 
         [Test]
         public void WhenSignOutActionCalled_ThenReturnsSignOutResult()
         {
-            _controller.Url = A.Fake<IUrlHelper>();
             IActionResult result = _controller.SignOut();
-
+            Assert.IsNotNull(result);
             Assert.IsInstanceOf<SignOutResult>(result);
         }
 
         [Test]
         public void WhenSignOutActionCalled_ThenUsesRequiredAuthenticationScheme()
         {
-            _controller.Url = A.Fake<IUrlHelper>();
+            string callbackUrl = "https://www.abc.com/";
+            A.CallTo(() => _fakeUrlHelper.Action(A<UrlActionContext>.Ignored)).Returns(callbackUrl);
+
             IActionResult result = _controller.SignOut();
 
             SignOutResult signOutResult = ((SignOutResult)result);
 
             Assert.IsInstanceOf<SignOutResult>(result);
+            Assert.AreEqual(callbackUrl, signOutResult.Properties.RedirectUri);
             Assert.IsTrue(signOutResult.AuthenticationSchemes.Contains(OpenIdConnectDefaults.AuthenticationScheme));
             Assert.IsTrue(signOutResult.AuthenticationSchemes.Contains(CookieAuthenticationDefaults.AuthenticationScheme));
         }
 
         [Test]
-        public void WhenSignedOutActionCalled_ThenSignout()
-        {  
-            _controller.Url = new UrlHelper(new ActionContext { RouteData = new RouteData() });
+        public void WhenSignedOutActionCalled_ThenRedirectResult()
+        {
+            A.CallTo(() => _fakeUrlHelper.Content(A<string>.Ignored)).Returns("test");
+
             IActionResult result = _controller.SignedOut();
 
-            Assert.IsInstanceOf<RedirectResult>(result);
             Assert.IsTrue(result != null);
+            Assert.IsInstanceOf<RedirectResult>(result);            
         }
     }
 }
