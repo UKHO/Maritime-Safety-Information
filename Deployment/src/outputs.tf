c84ebaabd7d7af344315c0dcbfc3451872b51295
Tejasi Chavan <tejasi15072@mastek.com>
2022-06-13 13:09:59 +0530

    updated admin webapp url in pipeline variables AB#48572 AB#49022

--- Deployment/src/outputs.tf
+++ Deployment/src/outputs.tf
@@ -15,7 +15,7 @@ output "Website_Url" {
 }
 
 output "Website_Admin_Url" {
-  value = "https://${module.webapp_service.default_site_hostname}/RadioNavigationalWarningsAdmin"
+  value = "https://${module.webapp_service.admin_default_site_hostname}/"
 }
 
 output "admin_webapp_name" {
