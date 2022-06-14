92ebd3873d403f85333a59082fceedbdce4be714
Mohammed Khan <mohamme15315@mastek.com>
2022-06-14 12:18:50 +0530

    Fixed review comments AB#47660

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/Controllers/HomeController.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/Controllers/HomeController.cs
@@ -9,10 +9,12 @@ namespace UKHO.MaritimeSafetyInformation.Web.Controllers
     public class HomeController : BaseController<HomeController>
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpContextAccessor _contextAccessor;
 
         public HomeController(IHttpContextAccessor contextAccessor, ILogger<HomeController> logger) : base(contextAccessor, logger)
         {
             _logger = logger;
+            _contextAccessor = contextAccessor;
         }
 
         public IActionResult Index()
@@ -25,10 +27,10 @@ namespace UKHO.MaritimeSafetyInformation.Web.Controllers
         [Route("/error")]
         public IActionResult Error()
         {
-            string correlationId = GetCurrentCorrelationId();
+            string correlationId = Convert.ToString(GetCurrentCorrelationId());
             ViewData["CurrentCorrelationId"] = correlationId;
-            IExceptionHandlerPathFeature exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-            _logger.LogError(EventIds.MSISystemError.ToEventId(), "System error has occurred while processing request with exception:{ex}, at exception path:{path} for correlationId:{correlationId}", exceptionDetails.Error.Message, exceptionDetails.Path, correlationId);
+            IExceptionHandlerPathFeature exceptionDetails = _contextAccessor.HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            _logger.LogError(EventIds.MSISystemError.ToEventId(), "System error has occurred while processing request with exception:{ex}, at exception path:{path} for correlationId:{correlationId}", exceptionDetails?.Error.Message, exceptionDetails?.Path, correlationId);
             return View();
         }
     }
