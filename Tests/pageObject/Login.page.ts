55f5f9aa47598edc84255ece84370f5526b4ad25
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-06-20 14:15:32 +0530

    45725 functional test AB#49043

--- Tests/pageObject/Login.page.ts
+++ Tests/pageObject/Login.page.ts
@@ -19,7 +19,7 @@ export default class Login {
     this.password = this.page.locator('[placeholder="Password"]');
     this.btnLogin = this.page.locator('button:has-text("Sign in")');
     this.loginUsername = this.page.locator('text=Test User');
-    this.signOut = this.page.locator('text=Sign out');
+    this.signOut = this.page.locator('#navbarSupportedContent > ul > li.nav-item.dropdown > ul > li > a');
     this.emailError= this.page.locator('div.error.itemLevel.show');
   }
 
@@ -41,8 +41,9 @@ export default class Login {
      {
       await Promise.all([
         this.page.waitForLoadState(),
-        this.signOut.click()]);
-        expect((await this.signIn.innerText()).toString()).toContain('Sign in')   
+        this.signOut.click(),
+        expect((await this.signIn.innerText()).toString()).toContain('Sign in')  
+      ]) 
      }
 }
 
