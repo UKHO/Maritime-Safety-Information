3ed3c6bf8ff24bf08d1319dfaa5224e2fd03988c
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-06-22 16:11:37 +0530

    47652 ui test automation AB#50328

---
+++ Tests/AutoTests/FunctionalTest/functional-ADLogin.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect, chromium, Page, Browser, BrowserContext } from '@playwright/test';
+
+import * as app from "../../Configuration/appConfig.json";
+import loginPage from '../../pageObject/Login.page';
+
+test.describe("AD Authentication Sign in For The maritime-safety-information", () => {
+  let login: loginPage;
+  test.beforeEach(async ({ page }) => {
+    await page.goto(app.rnwAdminUrl);
+    login = new loginPage(page);
+  });
+
+  test('With the Valid details', async ({ page, context }) => {
+    await login.adLogin();
+    await login.signout();
+  })
+  
+});
