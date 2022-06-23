206fbe9d2e14f35baaae7fbcec087fed5e496910
Ganpat Gawde <Ganpat14802@Mastek.Com>
2022-06-23 15:19:27 +0530

    45749 - added changes in js file AB#50344 AB#45749

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/wwwroot/js/Custom/RadioNavigationalWarnings.js
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/wwwroot/js/Custom/RadioNavigationalWarnings.js
@@ -17,26 +17,6 @@
     }
 }
 
-function showSelection() {
-    //var checkboxes = Array.from(document.querySelectorAll('input[type=checkbox]:checked'));
-
-    //var selectedIds = [];
-
-    ////for (var checkbox in checkboxes) {
-    ////    console.log(checkbox.getAttribute("warning-id"));
-    ////    //if (checkboxes[i].checked && i < checkboxes.length) {
-
-    ////       // selectedIds.push(((document.getElementById("Id_" + i)).innerHTML).trim());
-    ////   // }
-    ////}
-    //console.log(checkboxes);
-
-    //for (var i = 0; i < checkboxes.length; i++) {
-    //    selectedIds.push(checkboxes[i].getAttribute("warning-id"));
-    //}
-    //document.getElementById('showSelectionId').value = selectedIds
-}
-
 function enableDisableShowSelection() {
     var checkboxes = document.getElementsByName('checkbox');
     document.getElementById("BtnShowSelection").disabled = true;
@@ -47,3 +27,127 @@ function enableDisableShowSelection() {
         }
     }
 }
+
+let dropdownParent = document.querySelector('.site-header .dropdown');
+let dropdowToggleButton = document.querySelector('.site-header .dropdown-toggle');
+if (document.getElementById('hdnLoggedIn').value === "Y") {
+    dropdownParent.addEventListener('mouseenter', function () {
+        dropdowToggleButton.click();
+    })
+    dropdownParent.addEventListener('mouseleave', function () {
+        dropdowToggleButton.click();
+    })
+    //site header dropdown menu on mouse hover ends here//
+}
+const allwarningrows = Array.from(document.querySelectorAll(".rnw-allwarnings-table tbody tr"));
+const tableRef = document.querySelector(".rnw-allwarnings-table");
+const tableRefBody = document.querySelector(".rnw-allwarnings-table tbody");
+const select_button = document.querySelector("#select_button");
+let selected_tab;
+let selectedIds = [];
+
+let allWarningTabButton = document.querySelector('#allwarnings-tab');
+let navarea1TabButton = document.querySelector('#NAVAREA1-tab');
+let ukcostalTabButton = document.querySelector('#ukcoastal-tab');
+let rnwTabContent = document.querySelector('#rnwTabContent');
+let toggleDetailsButtons = Array.from(document.querySelectorAll('.rnw-allwarnings-table .accordion-button'));
+
+allWarningTabButton.addEventListener('click', function () {
+    selected_tab = undefined;
+    const rows = getFilteredWarningRows();
+    insertWarningRows(rows)
+})
+navarea1TabButton.addEventListener('click', function () {
+    selected_tab = "N";
+    const rows = getFilteredWarningRows("N");
+    insertWarningRows(rows)
+})
+ukcostalTabButton.addEventListener('click', function () {
+    selected_tab = "U";
+    const rows = getFilteredWarningRows("U");
+    insertWarningRows(rows)
+})
+
+toggleDetailsButtons.map(function (toggleDetailsButton, index) {
+
+    toggleDetailsButton.addEventListener('click', function (event) {
+
+        console.log(event.target, event.target.children);
+        resetViewdetailButtonText();
+        if (toggleDetailsButton.classList.contains("collapsed")) {
+            toggleDetailsButton.children[1].innerText = "View details";
+        }
+        else {
+            toggleDetailsButton.children[1].innerText = "Hide details";
+        }
+    })
+})
+
+function resetViewdetailButtonText() {
+    toggleDetailsButtons.map(function (toggleDetailsButton, index) {
+        if (toggleDetailsButton.classList.contains("collapsed")) {
+            toggleDetailsButton.children[1].innerText = "View details";
+        }
+        else {
+            toggleDetailsButton.children[1].innerText = "Hide details";
+        }
+    })
+
+}
+
+function insertWarningRows(rows) {
+    tableRefBody.innerHTML = '';
+
+    for (let value of rows) {
+        tableRef.tBodies[0].appendChild(value);
+    }
+
+    setTimeout(function () {
+        if (document.querySelector(".rnw-allwarnings-table .accordion-button:not(.collapsed)")) {
+            document.querySelector(".rnw-allwarnings-table .accordion-button:not(.collapsed)").click();
+        }
+    }, 100);
+    initEvents();
+}
+
+function getFilteredWarningRows(warningType) {
+    return allwarningrows.filter(
+        function (warning) {
+            return warningType ? warning.classList.contains(warningType) : warning;
+        }
+    );
+}
+
+function initEvents() {
+    const checkbox_warnings = Array.from(document.querySelectorAll(".checkbox_warning"));
+
+    checkbox_warnings.map(function (checkbox_warning) {
+        checkbox_warning.addEventListener("change", function (event) {
+            const id = event.target.getAttribute("warning-id");
+            if (selectedIds.indexOf(id) === -1) {
+                selectedIds.push(id);
+                event.target.getAttribute("warning-id")
+            }
+            else {
+                selectedIds.splice(id, 1);
+            }
+
+            document.getElementById('showSelectionId').value = selectedIds;
+        })
+
+    })
+}
+
+select_button.addEventListener("click", function (event) {
+    if (event.target.value === "Clear all") {
+        selectedIds = Array.from(document.querySelectorAll(".checkbox_warning")).map(function (element) {
+            return element.getAttribute("warning-id");
+        });
+    }
+    else {
+        selectedIds = [];
+    }
+    document.getElementById('showSelectionId').value = selectedIds;
+})
+
+initEvents();
