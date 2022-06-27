a0ccfc4cc36867d93182903885b8e7388b794dfd
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-06-27 20:30:20 +0530

    49191 ui test automation AB#50326

--- Tests/AutoTests/FunctionalTest/functional-weeklyfiledownload.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional-weeklyfiledownload.spec.ts
@@ -2,12 +2,15 @@ import { test, expect, chromium, Page, Browser, BrowserContext } from '@playwrig
 
 import * as app from "../../Configuration/appConfig.json";
 import noticeToMarinerWeekDownload from '../../pageObject/noticeToMarine-weekfiledownload.page';
+import noticeToMariner from '../../pageObject/noticetomarine.page';
 
 test.describe("Goto maritime-safety-information Notice To Mariner Page to Check The Weekly and Daily File Download", () => {
   let noticeFileDownload: noticeToMarinerWeekDownload;
+  let notice:noticeToMariner;
   test.beforeEach(async ({ page }) => {
     await page.goto(app.url);
     noticeFileDownload = new noticeToMarinerWeekDownload(page);
+    notice = new noticeToMariner(page);
   });
 
   test('Should Goto Notice to Mariner Page for Weekly Download', async ({ page, context }) => {
@@ -27,4 +30,18 @@ test.describe("Goto maritime-safety-information Notice To Mariner Page to Check 
     await noticeFileDownload.goToDailyFile();
     await noticeFileDownload.checkDailyFileDownload();
   })
+  test('Does the Notice to mariner Page url are displayed with page title',async ({page}) => {
+    await notice.checkpageurl('','Maritime Safety Information')
+    await notice.checkurl(notice.noticeMarine,'NoticesToMariners/Weekly','Notices to Mariners - Weekly')
+    await notice.checkurl(notice.tabdaily,'NoticesToMariners/Daily','Notices to Mariners - Daily')
+    await notice.checkurl(notice.tabcumulative,'NoticesToMariners/Cumulative','Notices to Mariners - Cumulative')
+    await notice.checkurl(notice.tabannual,'NoticesToMariners/Annual','Notices to Mariners - Annual')
+    await notice.checkurl(notice.menuLeisureFolios,'NoticesToMariners/Leisure','Notices to Mariners - Leisure')
+    await notice.checkurl(notice.menuValueAddedResellers,'NoticesToMariners/Resellers','Notices to Mariners - Value Added Resellers')
+    await notice.checkurl(notice.menuAbout,'NoticesToMariners/About','About Notices to Mariners')
+    await notice.checkurl(notice.radioNavigationalWarnings,'RadioNavigationalWarnings','Radio Navigational Warnings')
+    await notice.checkNavareaUrl(notice.navareatab,'RadioNavigationalWarnings','Radio Navigational Warnings - NAVAREA I')
+    await notice.checkUkcoastalUrl(notice.ukcoastaltab,'RadioNavigationalWarnings','Radio Navigational Warnings - UK Coastal')
+ })  
+
 });
