c669bf6dca894aa59e66c408366b4d9e28c52924
jigar15426 <Jeegar15426@IN-MH1LPW102766>
2022-06-27 20:53:36 +0530

    49191 ui test automation AB#50326

--- Tests/pageObject/noticetomarine.page.ts
+++ Tests/pageObject/noticetomarine.page.ts
@@ -66,19 +66,19 @@ export default class noticetoMarine
     public async checkurl(locator:Locator,url:string,title:string)
     {
         await locator.click();
-        await expect(this.page).toHaveURL(`${app.url}${url}`);
+        await expect(this.page).toHaveURL(`${app.url}/${url}`);
         await expect(this.page).toHaveTitle(title)
     }
     public async checkNavareaUrl(locator:Locator,url:string,title:string)
     {
         await locator.click();
-        await expect(this.page).toHaveURL(`${app.url}${url}#navarea1`);
+        await expect(this.page).toHaveURL(`${app.url}/${url}#navarea1`);
         await expect(this.page).toHaveTitle(title);
     }
     public async checkUkcoastalUrl(locator:Locator,url:string,title:string)
     {
         await locator.click();
-        await expect(this.page).toHaveURL(`${app.url}${url}#ukcoastal`);
+        await expect(this.page).toHaveURL(`${app.url}/${url}#ukcoastal`);
         await expect(this.page).toHaveTitle(title);
     }
    
