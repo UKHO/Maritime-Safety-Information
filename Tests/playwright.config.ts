5d5e715947e5d9bc8515b4b3d74f2a333c75ac6d
Pradeep I <pradeep15558@mastek.com>
2022-07-04 18:41:08 +0530

    45904 cross browser AB#50639

--- Tests/playwright.config.ts
+++ Tests/playwright.config.ts
@@ -8,6 +8,7 @@ const config: PlaywrightTestConfig = {
   expect: {
     timeout: 10000
   },
+  fullyParallel:true,
   forbidOnly: !!process.env.CI,
 
   retries: process.env.CI ? 2 : 0,
@@ -40,13 +41,7 @@ const config: PlaywrightTestConfig = {
         
       },
     },
-    {
-      name: 'safari',
-      use: {
-        ...devices['Desktop Safari'],
-        
-      },
-    },
+    
     
 
   ],
