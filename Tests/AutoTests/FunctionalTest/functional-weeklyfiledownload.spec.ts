b4111a1c8a499cb41ff756c649403df4ebfbc2e9
Pradeep I <pradeep15558@mastek.com>
2022-07-14 19:51:24 +0530

    50874 Automation coding AB#51232

--- Tests/AutoTests/FunctionalTest/functional-weeklyfiledownload.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional-weeklyfiledownload.spec.ts
@@ -38,7 +38,7 @@ test.describe("Goto maritime-safety-information Notice To Mariner Page to Check 
     expect((await name).length).toBeGreaterThan(0);
     var fileName = name[0];
     const newPageUrl = await (await page.$("[id^='download'] > a")).getAttribute('href');
-    expect(newPageUrl).toContain(`NoticesToMariners/DownloadWeeklyFile?fileName=${fileName}`);
+    expect(newPageUrl).toContain(`NoticesToMariners/DownloadFile?fileName=${fileName}`);
   })
 
   test('Should Goto Notices to Mariner Page for Daily download File', async ({ page, context }) => {
