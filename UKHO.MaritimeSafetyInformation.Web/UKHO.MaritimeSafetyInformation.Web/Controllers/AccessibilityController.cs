0e9fd694e2921ec492cedfb77010df25d4f081c3
Pradnya Puranik <Pradnya13705@Mastek.Com>
2022-07-19 12:51:25 +0530

    Accessibility statement screen added

---
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/Controllers/AccessibilityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UKHO.MaritimeSafetyInformation.Web.Controllers
+{
+    public class AccessibilityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
