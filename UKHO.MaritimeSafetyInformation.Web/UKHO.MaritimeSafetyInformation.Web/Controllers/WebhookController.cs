c4b357cffc38abd13d3c412935c613f3b91a4b81
Avinash15515 <avinash.patil@mastek.com>
2022-07-21 19:15:39 +0530

    AB#51492 Fix Data issue for Webhook

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/Controllers/WebhookController.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/Controllers/WebhookController.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using UKHO.MaritimeSafetyInformation.Common.Logging;
 
 namespace UKHO.MaritimeSafetyInformation.Web.Controllers
@@ -29,13 +28,13 @@ namespace UKHO.MaritimeSafetyInformation.Web.Controllers
 
         [HttpPost]
         [Route("/webhook/newfilespublished")]
-        public virtual async Task<IActionResult> NewFilesPublished([FromBody] JObject request)
+        public virtual async Task<IActionResult> NewFilesPublished()
         {
             ////Adding payload to log temporarily so that in can be used for unit testing etc. 
-            string payload = request.ToString(Formatting.None);
+            using var reader = new StreamReader(Request.Body, Encoding.UTF8);
+            string payload = await reader.ReadToEndAsync();
             Logger.LogInformation(EventIds.ClearFSSSearchCacheEventStart.ToEventId(), "Clear FSS Search Cache Event started for _X-Correlation-ID:{correlationId} Payload: {payload}", GetCurrentCorrelationId(), payload);
-
-            return GetCacheResponse(); 
+            return GetCacheResponse();
         }
     }
 }
