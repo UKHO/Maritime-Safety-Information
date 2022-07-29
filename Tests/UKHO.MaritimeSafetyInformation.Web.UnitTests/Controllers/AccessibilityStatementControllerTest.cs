baf7449c1b0301ec59e5e1b2e6579bb8768dc137
pradnya13705 <84022908+pradnya13705@users.noreply.github.com>
2022-07-29 21:02:15 +0530

    52085 Add accessibility statement page (#81)
    Co-authored-by: Pradnya Puranik <Pradnya13705@Mastek.Com>
    Co-authored-by: usha13529 <usha13529@Mastek.com>
    Co-authored-by: Ganpat Gawde <Ganpat14802@Mastek.Com>

---
+++ Tests/UKHO.MaritimeSafetyInformation.Web.UnitTests/Controllers/AccessibilityStatementControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using UKHO.MaritimeSafetyInformation.Web.Controllers;
+
+namespace UKHO.MaritimeSafetyInformation.Web.UnitTests.Controllers
+{
+    [TestFixture]
+    public class AccessibilityStatementControllerTest
+    {
+        private AccessibilityStatementController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _controller = new AccessibilityStatementController();
+        }
+
+        [Test]
+        public void  WhenICallIndexView_ThenReturnView()
+        {
+            IActionResult result = _controller.Index();
+
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+    }
+}
