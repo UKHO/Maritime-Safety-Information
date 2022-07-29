90132e2798921738a2b78e92399bc7777a5c13c7
Avinash15515 <103419359+Avinash15515@users.noreply.github.com>
2022-07-29 21:37:14 +0530

    51492 Clear the FSS Cache (#83)
    Co-authored-by: Ganpat Gawde <Ganpat14802@Mastek.Com>
    Co-authored-by: Tejasi Chavan <tejasi15072@mastek.com>
    Co-authored-by: Mubeen Patel <mubeen13684@mastek.com>

---
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Models/WebhookRequest/FSSNewFilesPublishedEventData.cs
@@ -0,0 +1,50 @@
+﻿namespace UKHO.MaritimeSafetyInformation.Common.Models.WebhookRequest
+{
+    public class BatchDetails
+    {
+        public string Href { get; set; }
+    }
+
+    public class BatchStatus
+    {
+        public string Href { get; set; }
+    }
+
+    public class Links
+    {
+        public BatchDetails BatchDetails { get; set; }
+        public BatchStatus BatchStatus { get; set; }
+        public Get Get { get; set; }
+    }
+
+    public class Attribute
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class Get
+    {
+        public string Href { get; set; }
+    }
+
+    public class File
+    {
+        public Links Links { get; set; }
+        public string Hash { get; set; }
+        public int FileSize { get; set; }
+        public string MimeType { get; set; }
+        public string Filename { get; set; }
+        public List<Attribute> Attributes { get; set; }
+    }
+
+    public class FSSNewFilesPublishedEventData
+    {
+        public Links Links { get; set; }
+        public string BusinessUnit { get; set; }
+        public List<Attribute> Attributes { get; set; }
+        public List<File> Files { get; set; }
+        public string BatchId { get; set; }
+        public DateTime? BatchPublishedDate { get; set; }
+    }
+}
---
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web/Validation/EnterpriseEventCacheDataRequestValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System.Net;
+using UKHO.MaritimeSafetyInformation.Common.Models.WebhookRequest;
+
+namespace UKHO.MaritimeSafetyInformation.Web.Validation
+{
+
+    public interface IEnterpriseEventCacheDataRequestValidator
+    {
+        Task<ValidationResult> Validate(FSSNewFilesPublishedEventData enterpriseEventCacheDataRequest);
+    }
+
+    public class EnterpriseEventCacheDataRequestValidator : AbstractValidator<FSSNewFilesPublishedEventData>, IEnterpriseEventCacheDataRequestValidator
+    {
+        
+        public EnterpriseEventCacheDataRequestValidator()
+        {
+            RuleFor(v => v.BusinessUnit).NotNull().NotEmpty()
+                .WithErrorCode(HttpStatusCode.OK.ToString())
+                .WithMessage("Business unit cannot be blank or null.");
+
+            RuleFor(v => v.Attributes).NotNull().NotEmpty()
+                .WithErrorCode(HttpStatusCode.OK.ToString())
+                .WithMessage("Attributes cannot be blank or null.");
+
+            RuleFor(b => b.Attributes)
+              .Must(at => at.All(a => !string.IsNullOrWhiteSpace(a.Key) && !string.IsNullOrWhiteSpace(a.Value))).OverridePropertyName("Attributes")
+              .When(ru => ru.Attributes != null)
+              .WithErrorCode(HttpStatusCode.OK.ToString())
+              .WithMessage("Attribute key or value cannot be null.");
+        }
+        Task<ValidationResult> IEnterpriseEventCacheDataRequestValidator.Validate(FSSNewFilesPublishedEventData enterpriseEventCacheDataRequest)
+        {
+            return ValidateAsync(enterpriseEventCacheDataRequest);
+        }
+    }
+}
