f8a2ebb95e6c67477952138546d4b559933074a1
Shirin14926 <Shirin.talawdekar@mastek.com>
2022-08-04 19:13:23 +0530

    51158 remove instrumentation key AB#51158

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Configuration/FileShareServiceConfiguration.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Configuration/FileShareServiceConfiguration.cs
@@ -9,8 +9,9 @@ namespace UKHO.MaritimeSafetyInformation.Common.Configuration
         public string BusinessUnit { get; set; }
         public string ProductType { get; set; }
         public string FssClientId { get; set; }
+        public int MaxAttributeValuesCount { get; set; }
         public int PageSize { get; set; }
         public int Start { get; set; }
-        public int MaxAttributeValuesCount { get; set; }
+        
     }
 }
