e4d10fcb4ce3945cd1006a8b135017258a0f211d
Tejasi Chavan <tejasi15072@mastek.com>
2022-08-04 11:52:04 +0530

    Updated table name for storage account AB#52362 AB#53843

--- Deployment/src/main.tf
+++ Deployment/src/main.tf
@@ -88,7 +88,7 @@ module "azure-dashboard" {
 module "cache_storage" {
   source                                = "./Modules/CacheStorage"
   name                                  = "${local.service_name}${local.env_name}cachestorage"
-  table_name                            = "${local.service_name}${local.env_name}bannernotificationtable"
+  table_name                            = "MsiBannerNotificationTable"
   resource_group_name                   = azurerm_resource_group.rg.name
   allowed_ips                           = var.allowed_ips
   location                              = azurerm_resource_group.rg.location
