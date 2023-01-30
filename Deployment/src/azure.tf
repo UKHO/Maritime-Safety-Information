d0e72ef0779a88fe132d0ec89fe92a22f8f86d1c
Jivitesh Trivedi <ajivitesh@ukho.gov.uk>
2023-01-30 16:24:23 +0000

    Abzu-changes to run main branch build (after creating new temp branch from main)

--- Deployment/src/azure.tf
+++ Deployment/src/azure.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.1.0"
+      version = "=3.39.1"
     }
   }
 
-  required_version = "=1.1.8"
+  required_version = "=1.3.7"
   backend "azurerm" {
     container_name = "tfstate"
     key            = "terraform.deployment.tfplan"
