9c391c08e96d3d3a9b019ccf72b898b2a81fb342
Jivitesh Trivedi <ajivitesh@ukho.gov.uk>
2023-04-24 09:58:30 +0100

    Abzu-Fixed tests

--- Tests/playwright.config.ts
+++ Tests/playwright.config.ts
@@ -3,33 +3,33 @@ import { devices } from '@playwright/test';
 
 
 const config: PlaywrightTestConfig = {
-  testDir: './AutoTests',
-  timeout: 30 * 3000,
-  expect: {
-    timeout: 10000
-  },
-  globalTimeout: 60 * 60 * 1000,
-  forbidOnly: !!process.env.CI,
-
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: [ ['junit', { outputFile: 'results.xml' }] ],
-  use: {
-    actionTimeout: 10 * 1000,
-    navigationTimeout: 30 * 1000,
-    trace: 'on-first-retry',
-    
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        
-      },
+    testDir: './AutoTests',
+    timeout: 100 * 1000,
+    expect: {
+        timeout: 10000
     },
+    globalTimeout: 60 * 60 * 1000,
+    forbidOnly: !!process.env.CI,
 
-  ],
+    retries: 3,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: [['junit', { outputFile: 'results.xml' }]],
+    use: {
+        actionTimeout: 10 * 1000,
+        navigationTimeout: 150 * 1000,
+        trace: 'on-first-retry',
+
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: {
+                ...devices['Desktop Chrome'],
+
+            },
+        },
+
+    ],
 
 };
 
