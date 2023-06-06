4be9f824be0f52ff96e700780f0dd0bacebe4e20
JiviteshT <112479261+JiviteshT@users.noreply.github.com>
2023-06-06 19:25:47 +0100

    Abzu-82335-removed swift connection resource and used inline subnet id property. (#114)

--- Deployment/src/azure.tf
+++ Deployment/src/azure.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.39.1"
+      version = "=3.55.0"
     }
   }
 
-  required_version = "=1.3.7"
+  required_version = "=1.4.5"
   backend "azurerm" {
     container_name = "tfstate"
     key            = "terraform.deployment.tfplan"
