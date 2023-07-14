411aba4066f5b2fea9a8d437d834b3bdd069621f
Hugo Burgess <hugo.burgess@ukho.gov.uk>
2023-07-14 14:50:32 +0100

    Abzu 67067 - update functional tests

--- Tests/AutoTests/FunctionalTest/functional-RadioNavigationalWarningsCreateRNW.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional-RadioNavigationalWarningsCreateRNW.spec.ts
@@ -3,66 +3,72 @@ import * as app from "../../Configuration/appConfig.json";
 import RadioNavigationalWarnings from '../../pageObject/RadioNavigationalWarnings.page';
 import loginPage from '../../pageObject/Login.page';
 
-test.describe("Create new radio navigational warnings record", ()=> {
-  let radioNavigationalWarnings:RadioNavigationalWarnings;
-  let login: loginPage;
+test.describe("Create new radio navigational warnings record", () => {
+    let radioNavigationalWarnings: RadioNavigationalWarnings;
+    let login: loginPage;
 
-  test.beforeEach(async ({page}) => {
-    await page.goto(app.rnwAdminUrl); 
-    login = new loginPage(page);
-    await login.adLogin(app.RNWAdminAutoTest_User,app.RNWAdminAutoTest_Pass);
-    radioNavigationalWarnings = new RadioNavigationalWarnings(page);  
-  });
+    test.beforeEach(async ({ page }) => {
+        await page.goto(app.rnwAdminUrl);
+        login = new loginPage(page);
+        await login.adLogin(app.RNWAdminAutoTest_User, app.RNWAdminAutoTest_Pass);
+        radioNavigationalWarnings = new RadioNavigationalWarnings(page);
+    });
 
-  test('Without entered input fields',async({page})=>{
-    await radioNavigationalWarnings.pageLoad();
-    await radioNavigationalWarnings.createRNW();   
-    await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.warningError,"The Warning Type field is required.") ;
-    await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.referenceEror,"The Reference field is required.");
-    await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.contentError,'The Text field is required.');
-    await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.datetimeError,"The Date Time Group field is required.");
-    await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.summaryError,"The Description field is required."); 
-    
-  })  
+    test('Without entered input fields', async ({ page }) => {
+        await radioNavigationalWarnings.pageLoad();
+        await radioNavigationalWarnings.createRNW();
+        await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.warningError, "The Warning Type field is required.");
+        await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.referenceEror, "The Reference field is required.");
+        await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.contentError, 'The Text field is required.');
+        await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.datetimeError, "The Date Time Group field is required.");
+        await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.summaryError, "The Description field is required.");
+    })
 
-   test('With content text as blank',async({page})=>{   
-    await radioNavigationalWarnings.pageLoad();
-    await radioNavigationalWarnings.fillFormWithValidDetails("1","");  
-    await radioNavigationalWarnings.createRNW();
-    await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.contentError,'The Text field is required.'); 
-  }) 
- 
-   test('With valid input details with Navarea',async({page})=>{   
-    await radioNavigationalWarnings.pageLoad();
-    await radioNavigationalWarnings.fillFormWithValidDetails("1","testdata");  
-    await radioNavigationalWarnings.createRNW(); 
-    await radioNavigationalWarnings.confirmationBox(radioNavigationalWarnings.alertMessage,radioNavigationalWarnings.message,"yes");
-    await radioNavigationalWarnings.getDialogText('Record created successfully!')    
-  })
+    test('With content text as blank', async ({ page }) => {
+        await radioNavigationalWarnings.pageLoad();
+        await radioNavigationalWarnings.fillFormWithValidDetails("1", "");
+        await radioNavigationalWarnings.createRNW();
+        await radioNavigationalWarnings.checkErrorMessage(radioNavigationalWarnings.contentError, 'The Text field is required.');
+    })
 
-   test('With valid input details with UK costal',async({page})=>{     
-    await radioNavigationalWarnings.pageLoad();
-    await radioNavigationalWarnings.fillFormWithValidDetails("2","testdata");  
-    await radioNavigationalWarnings.createRNW(); 
-    await radioNavigationalWarnings.confirmationBox(radioNavigationalWarnings.alertMessage,radioNavigationalWarnings.message,"yes");
-    await radioNavigationalWarnings.getDialogText('Record created successfully!');    
-  })
+    test('With valid input details with Navarea', async ({ page }) => {
+        await radioNavigationalWarnings.pageLoad();
+        await radioNavigationalWarnings.fillFormWithValidDetails("1", "testdata");
+        await radioNavigationalWarnings.createRNW();
+        await radioNavigationalWarnings.getDialogText('Record created successfully!')
+    })
 
-  test('With valid input check for duplicate',async({page})=>{
+    test('With valid input details with UK costal', async ({ page }) => {
+        await radioNavigationalWarnings.pageLoad();
+        await radioNavigationalWarnings.fillFormWithValidDetails("2", "testdata");
+        await radioNavigationalWarnings.createRNW();
+        await radioNavigationalWarnings.getDialogText('Record created successfully!');
+    })
 
-    await radioNavigationalWarnings.pageLoad();
-    await radioNavigationalWarnings.fillFormWithValidDetails("1","testdata");
-    await radioNavigationalWarnings.createRNW();
-    await radioNavigationalWarnings.confirmationBox(radioNavigationalWarnings.alertMessage,radioNavigationalWarnings.message,"no")
-    
+    test('With valid input check for duplicate and accept', async ({ page }) => {
 
+        await radioNavigationalWarnings.pageLoad();
+        await radioNavigationalWarnings.fillFormWithValidDetails("1", "testdata");
+        await radioNavigationalWarnings.createRNW();
+        await radioNavigationalWarnings.confirmationBox(radioNavigationalWarnings.alertMessage, radioNavigationalWarnings.message, "yes")
+        await radioNavigationalWarnings.getDialogText('Record created successfully!');
+    })
 
+    test('With valid input check for duplicate and reject', async ({ page }) => {
 
-  })
+        await radioNavigationalWarnings.pageLoad();
+        await radioNavigationalWarnings.fillFormWithValidDetails("1", "testdata");
+        await radioNavigationalWarnings.createRNW();
+        await radioNavigationalWarnings.confirmationBox(radioNavigationalWarnings.alertMessage, radioNavigationalWarnings.message, "no")
+        await radioNavigationalWarnings.checkConfirmationBoxVisible(false)
+    })
 
+    test('With valid input check for duplicate and cancel', async ({ page }) => {
+
+        await radioNavigationalWarnings.pageLoad();
+        await radioNavigationalWarnings.fillFormWithValidDetails("1", "testdata");
+        await radioNavigationalWarnings.createRNW();
+        await radioNavigationalWarnings.confirmationBox(radioNavigationalWarnings.alertMessage, radioNavigationalWarnings.message, "cancel")
+        await radioNavigationalWarnings.checkConfirmationBoxVisible(false)
+    })
 });
-  
-
-  
-
-
--- Tests/pageObject/RadioNavigationalWarnings.page.ts
+++ Tests/pageObject/RadioNavigationalWarnings.page.ts
@@ -1,96 +1,92 @@
 import { expect } from '@playwright/test';
 import type { Locator, Page } from 'playwright';
 
-export default class RadioNavigationalWarnings
-{
-    
-    private page:Page;
-    readonly reference:Locator;
-    readonly datetime:Locator;
-    readonly description:Locator;
-    readonly createNewRecord:Locator;    
-    readonly content:Locator;
-    readonly create:Locator;
-    readonly warning:Locator;
-    readonly warningError:Locator;
-    readonly referenceEror:Locator;
-    readonly datetimeError:Locator;
-    readonly summaryError:Locator;
-    readonly contentError:Locator;
-    readonly editNewRecord:Locator;
-    readonly warningType:Locator;
-    readonly year:Locator;
-    readonly filter:Locator;
-    readonly delete:Locator;
-    readonly edit:Locator;
-    readonly expiryDate:Locator;
-    readonly alertMessage:Locator; 
-    readonly confirmAlertNo:Locator;
-    readonly confirmAlertYes:Locator;
-    readonly message="A warning record with this reference number already exists. Would you like to add another record with the same reference?"
+export default class RadioNavigationalWarnings {
 
+    private page: Page;
+    readonly reference: Locator;
+    readonly datetime: Locator;
+    readonly description: Locator;
+    readonly createNewRecord: Locator;
+    readonly content: Locator;
+    readonly create: Locator;
+    readonly warning: Locator;
+    readonly warningError: Locator;
+    readonly referenceEror: Locator;
+    readonly datetimeError: Locator;
+    readonly summaryError: Locator;
+    readonly contentError: Locator;
+    readonly editNewRecord: Locator;
+    readonly warningType: Locator;
+    readonly year: Locator;
+    readonly filter: Locator;
+    readonly delete: Locator;
+    readonly edit: Locator;
+    readonly expiryDate: Locator;
+    readonly alertMessage: Locator;
+    readonly overlay: Locator;
+    readonly backdrop: Locator;
+    readonly confirmAlertClose: Locator;
+    readonly confirmAlertNo: Locator;
+    readonly confirmAlertYes: Locator;
+    readonly message = "A warning record with this reference number already exists. Would you like to add another record with the same reference?"
 
-    constructor(page:Page)
-    {
-        this.page = page; 
+    constructor(page: Page) {
+        this.page = page;
         this.reference = this.page.locator('#Reference');
-        this.datetime= this.page.locator('#DateTimeGroup');
-        this.description =  this.page.locator('#Summary');
+        this.datetime = this.page.locator('#DateTimeGroup');
+        this.description = this.page.locator('#Summary');
         this.createNewRecord = this.page.locator('text=Create New');
-        this.content=this.page.locator('#Content');
-        this.create= this.page.locator('#btnCreate');
+        this.content = this.page.locator('#Content');
+        this.create = this.page.locator('#btnCreate');
         this.warning = this.page.locator('#WarningType');
-        this.warningError=  this.page.locator("#WarningType-error");
-        this.referenceEror= this.page.locator("#Reference-error");
-        this.datetimeError= this.page.locator("#DateTimeGroup-error");
-        this.summaryError= this.page.locator("#Summary-error");
-        this.contentError=this.page.locator("#Content-error");
+        this.warningError = this.page.locator("#WarningType-error");
+        this.referenceEror = this.page.locator("#Reference-error");
+        this.datetimeError = this.page.locator("#DateTimeGroup-error");
+        this.summaryError = this.page.locator("#Summary-error");
+        this.contentError = this.page.locator("#Content-error");
         this.editNewRecord = this.page.locator("#btnEdit");
         this.warningType = this.page.locator("#WarningType");
-        this.year= this.page.locator("#Year");
+        this.year = this.page.locator("#Year");
         this.filter = this.page.locator("#BtnFilter");
         this.delete = this.page.locator('#IsDeleted');
-        this.edit=this.page.locator("[id^='Edit'] > a");
-        this.expiryDate=this.page.locator("#ExpiryDate");
-        this.alertMessage=this.page.locator(".modal-body");
-        this.confirmAlertNo=this.page.locator("#modalConfirmNoId");
-        this.confirmAlertYes=this.page.locator("#modalConfirmYesId");
+        this.edit = this.page.locator("[id^='Edit'] > a");
+        this.expiryDate = this.page.locator("#ExpiryDate");
+        this.alertMessage = this.page.locator(".modal-body");
+        this.overlay = this.page.locator("#overlayId");
+        this.backdrop = this.page.locator("#backdropId");
+        this.confirmAlertClose = this.page.locator("#modalConfirmCloseId");
+        this.confirmAlertNo = this.page.locator("#modalConfirmNoId");
+        this.confirmAlertYes = this.page.locator("#modalConfirmYesId");
     }
 
-    public async SelectRadioNavigationalWarning()
-    {
+    public async SelectRadioNavigationalWarning() {
         await this.createNewRecord.click();
         await expect(this.warning).toBeTruthy();
-    }  
-   
-    public async createRNW()
-    {
-        await  this.create.click();
     }
 
-    public async isDelete()
-    {
+    public async createRNW() {
+        await this.create.click();
+    }
+
+    public async isDelete() {
         await this.delete.uncheck();
         await expect(this.warning).toBeTruthy();
     }
 
-    public async getDialogText(text:string)
-    {
+    public async getDialogText(text: string) {
         await this.page.on('dialog', async (dialog) => {
-        expect(dialog.message()).toEqual(text);
-        dialog.accept();
-        })     
-    }   
-
-    public async checkErrorMessage(locator:Locator,text:String)
-    {
-        await this.page.waitForLoadState();
-        expect((await locator.textContent()).toString()).toEqual(text);   
+            expect(dialog.message()).toEqual(text);
+            dialog.accept();
+        })
     }
-    
-    
-    public async fillFormWithValidDetails(warningType:string,content:string)
-    {
+
+    public async checkErrorMessage(locator: Locator, text: String) {
+        await this.page.waitForLoadState();
+        expect((await locator.textContent()).toString()).toEqual(text);
+    }
+
+    public async fillFormWithValidDetails(warningType: string, content: string) {
         await this.warning.selectOption(warningType);
         await this.reference.fill("reference");
         await this.page.waitForTimeout(3000);
@@ -100,66 +96,73 @@ export default class RadioNavigationalWarnings
         await this.content.fill(content);
         await this.expiryDate.type("05072022 06000");
     }
-    public async clearInput(locator:Locator)
-    {
+
+    public async clearInput(locator: Locator) {
         const input = await locator;
         await input.click({ clickCount: 3 })
-        await this.page.keyboard.press('Control+A')   
-        await this.page.keyboard.press('Backspace')     
-    }
-    public async editRNW()
-    {
-       await this.editNewRecord.click();
+        await this.page.keyboard.press('Control+A')
+        await this.page.keyboard.press('Backspace')
     }
 
-    public async getEditUrl()
-    {
+    public async editRNW() {
+        await this.editNewRecord.click();
+    }
+
+    public async getEditUrl() {
         const newEdit = await this.edit.first();
-        await newEdit.click(); 
+        await newEdit.click();
     }
-    public async pageLoad()
-    {
+
+    public async pageLoad() {
         const [response] = await Promise.all([
-           this.page.waitForNavigation(), 
-           this.createNewRecord.click(),
-           ]);
+            this.page.waitForNavigation(),
+            this.createNewRecord.click(),
+        ]);
     }
-    public async searchListWithfilter(selectWarnings:string)
-    {
-  
-      await this.warningType.selectOption({ label: selectWarnings });
-      await this.filter.click();
-     
+
+    public async searchListWithfilter(selectWarnings: string) {
+
+        await this.warningType.selectOption({ label: selectWarnings });
+        await this.filter.click();
     }
-    public async fillEditFormWithValidDetails(content:string)
-    {
+
+    public async fillEditFormWithValidDetails(content: string) {
         await this.reference.fill("reference");
         await this.page.waitForTimeout(3000);
         await this.datetime.type("05072022 05533");
         await this.page.keyboard.press("ArrowDown");
         await this.description.fill("testdata");
         await this.content.type(content);
-
     }
 
-    public async confirmationBox(locator:Locator,text:String,alert:String)
-    {   
+    public async confirmationBox(locator: Locator, text: String, alert: String) {
         await this.page.waitForLoadState();
 
-            expect((await locator.textContent()).toString().trim()).toEqual(text);
-        if(alert === 'yes'){
-           
+        expect((await locator.textContent()).toString().trim()).toEqual(text);
+
+        if (alert === 'yes') {
+
             await this.confirmAlertYes.click();
         }
-        else{
-            
+        else if (alert === 'no') {
+
             await this.confirmAlertNo.click();
         }
-        
-        
+        else {
 
-
+            await this.confirmAlertClose.click();
+        }
     }
 
-  
+    public async checkConfirmationBoxVisible(visible: boolean) {
+        await this.page.waitForLoadState();
+
+        if (visible) {
+            await expect(this.overlay.toHaveClass(/d-block/));
+            await expect(this.backdrop.toHaveClass(/d-block/));
+        } else {
+            await expect(this.overlay.toHaveClass(/d-none/));
+            await expect(this.backdrop.toHaveClass(/d-none/));
+        }
+    }
 }
