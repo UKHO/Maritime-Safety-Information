adb241a81792d54b07b7fdd9944c40ea4aa97e4e
Hugo Burgess <hugo.burgess@ukho.gov.uk>
2023-07-19 13:06:09 +0100

    Abzu 87101 - update functional tests

--- Tests/AutoTests/FunctionalTest/functional-ADLogin.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional-ADLogin.spec.ts
@@ -1,25 +1,26 @@
 import { test, expect, chromium, Page, Browser, BrowserContext } from '@playwright/test';
-
 import * as app from "../../Configuration/appConfig.json";
 import loginPage from '../../pageObject/Login.page';
 
 test.describe("AD Authentication Sign in For The maritime-safety-information admin app", () => {
-  let login: loginPage;
-  test.beforeEach(async ({ page }) => {
-    await page.goto(app.rnwAdminUrl);
-    login = new loginPage(page);
-  });
+    let login: loginPage;
+    test.beforeEach(async ({ page }) => {
+        await page.goto(app.rnwAdminUrl);
+        login = new loginPage(page);
+    });
 
-  test('With the Valid details', async ({ page, context }) => {
-    await login.adLogin(app.RNWAdminAutoTest_User,app.RNWAdminAutoTest_Pass);
-    await login.adsignout();
-  })
-  test('With the Unauthorised details', async ({ page, context }) => {
-    await login.adLogin(app.RNWAdminAutoTestNoAccess_User,app.RNWAdminAutoTestNoAccess_Pass);
-    await login.adUnathoriseddetails();
-  })
-  test('With the Invalid details', async ({ page, context }) => {
-    await login.adLogin(app.RNWAdminAutoTest_User,'1111111');
-    await login.adpassworError();
-  })
+    test('With the Valid details', async ({ page, context }) => {
+        await login.adLogin(app.RNWAdminAutoTest_User, app.RNWAdminAutoTest_Pass);
+        await login.adSignOut();
+    })
+
+    test('With the Unauthorised details', async ({ page, context }) => {
+        await login.adLogin(app.RNWAdminAutoTestNoAccess_User, app.RNWAdminAutoTestNoAccess_Pass);
+        await login.adUnathorisedDetails();
+    })
+
+    test('With the Invalid details', async ({ page, context }) => {
+        await login.adLogin(app.RNWAdminAutoTest_User, '1111111');
+        await login.adPasswordErrorCheck();
+    })
 });
--- Tests/pageObject/Login.page.ts
+++ Tests/pageObject/Login.page.ts
@@ -2,119 +2,103 @@ import { expect } from '@playwright/test';
 import type { Locator, Page } from 'playwright';
 
 export default class Login {
-  private page: Page;
-  readonly signIn: Locator;
-  readonly username: Locator;
-  readonly btnContinue: Locator;
-  readonly password: Locator;
-  readonly btnLogin: Locator;
-  readonly loginUsername: Locator;
-  readonly signOut: Locator;
-  readonly emailError:Locator;
-  readonly adUsername:Locator;
-  readonly adNext:Locator;
-  readonly adPassword:Locator;
-  readonly login:Locator;
-  readonly adYes:Locator;
-  readonly adUserNameDropdown:Locator;
-  readonly adSignOutText:Locator;
-  readonly adPasswordError:Locator;
-  readonly adUnathorisedError:Locator;
-  constructor(page: Page) {
-    this.page = page;
-    this.signIn = this.page.locator('a:has-text("Sign in")');
-    this.username = this.page.locator('[placeholder="Email"]');
-    this.btnContinue = this.page.locator('[aria-label="Continue"]');
-    this.password = this.page.locator('[placeholder="Password"]');
-    this.btnLogin = this.page.locator('button:has-text("Sign in")');
-    this.loginUsername = this.page.locator('text=UKHOTest MSI');
-    this.signOut = this.page.locator('text=Sign out');
-    this.emailError= this.page.locator('div.error.itemLevel.show');
-    this.adUsername = this.page.locator('[placeholder="someone\\@ukho\\.gov\\.uk"]');
-    this.adNext=  this.page.locator('text=Next');
-    this.adPassword = this.page.locator('[placeholder="Password"]');
-    this.login = this.page.locator('text=Sign in');
-    this.adYes=this.page.locator('#idSIButton9');
-    this.adUserNameDropdown=this.page.locator('#navbarDropdown');
-    this.adSignOutText=this.page.locator("#navbarSupportedContent > ul > li > ul > li > a")
-    this.adPasswordError=this.page.locator("#passwordError")
-    this.adUnathorisedError=this.page.locator("#headingLevelOne")
-  }
+    private page: Page;
+    readonly signIn: Locator;
+    readonly username: Locator;
+    readonly btnContinue: Locator;
+    readonly password: Locator;
+    readonly btnLogin: Locator;
+    readonly loginUsername: Locator;
+    readonly signOutText: Locator;
+    readonly emailError: Locator;
+    readonly adUsername: Locator;
+    readonly adNext: Locator;
+    readonly adPassword: Locator;
+    readonly login: Locator;
+    readonly adYes: Locator;
+    readonly adUserNameDropdown: Locator;
+    readonly adSignOutText: Locator;
+    readonly adPasswordError: Locator;
+    readonly adUnathorisedError: Locator;
 
-  public async goToSignIn() {
-    await this.signIn.click();
-  }
- 
-  public async loginWithValidDetails(username: string, password: string) {
-    await this.username.click();
-    await this.username.fill(username);
-    await this.btnContinue.click();
-    await this.password.fill(password);
-    await this.btnLogin.click();
-    await this.loginUsername.click();
-      expect((await this.loginUsername.innerText()).toString()).toContain("UKHOTest MSI");
-     }
-
-     public async loginWithDistributorDetails(username: string, password: string) {
-      await this.username.click();
-      await this.username.fill(username);
-      await this.btnContinue.click();
-      await this.password.fill(password);
-      await this.btnLogin.click();
-      
-      
-       } 
-
-     public async signout()
-     {
-      await Promise.all([
-        this.page.waitForLoadState(),
-        this.loginUsername.click(),
-        this.signOut.click(),
-        this.page.waitForLoadState(),
-        expect((await this.signIn.innerText()).toString()).toContain('Sign in')])
-     }
-
-     public async adSignout()
-     {
-      await Promise.all([
-        this.page.waitForLoadState(),
-        this.loginUsername.click(),
-        this.signOut.click(),
-      ])
+    constructor(page: Page) {
+        this.page = page;
+        this.signIn = this.page.locator('a:has-text("Sign in")');
+        this.username = this.page.locator('[placeholder="Email"]');
+        this.btnContinue = this.page.locator('[aria-label="Continue"]');
+        this.password = this.page.locator('[placeholder="Password"]');
+        this.btnLogin = this.page.locator('button:has-text("Sign in")');
+        this.loginUsername = this.page.locator('text=UKHOTest MSI');
+        this.signOutText = this.page.locator('text=Sign out');
+        this.emailError = this.page.locator('div.error.itemLevel.show');
+        this.adUsername = this.page.locator('[placeholder="someone\\@ukho\\.gov\\.uk"]');
+        this.adNext = this.page.locator('text=Next');
+        this.adPassword = this.page.locator('[placeholder="Password"]');
+        this.login = this.page.locator('text=Sign in');
+        this.adYes = this.page.locator('#idSIButton9');
+        this.adUserNameDropdown = this.page.locator('#navbarDropdown');
+        this.adSignOutText = this.page.locator("#navbarSupportedContent > ul > li > ul > li > a")
+        this.adPasswordError = this.page.locator("#passwordError")
+        this.adUnathorisedError = this.page.locator("#headingLevelOne")
     }
 
-    public async adLogin(username: string, password: string)
-    {
-     await this.adUsername.fill(username);
-     await Promise.all([
-     this.page.waitForNavigation(),
-     this.adNext.click()
-     ]);
-     await Promise.all([
-      this.adPassword.fill(password),  
-     ]);
-     await Promise.all([
-     this.page.waitForNavigation(),
-     this.login.click()
-     ]);
-    
+    public async goToSignIn() {
+        await this.signIn.click();
     }
-     public async adsignout()
-     {
-      await this.adUserNameDropdown.click();
-      expect(await this.adSignOutText.innerText()).toContain('Sign out')
-     }
-     public async adUnathoriseddetails()
-     {
-      expect(await this.adUnathorisedError.innerText()).toContain("Sorry, you do not have access to this website.");
-     }
-     public async adpassworError()
-     {
-      expect(await this.adPasswordError.innerText()).toContain('Your account or password is incorrect');
-     }
+
+    public async loginWithValidDetails(username: string, password: string) {
+        await this.username.click();
+        await this.username.fill(username);
+        await this.btnContinue.click();
+        await this.password.fill(password);
+        await this.btnLogin.click();
+        await this.loginUsername.click();
+        expect((await this.loginUsername.innerText()).toString()).toContain("UKHOTest MSI");
+    }
+
+    public async loginWithDistributorDetails(username: string, password: string) {
+        await this.username.click();
+        await this.username.fill(username);
+        await this.btnContinue.click();
+        await this.password.fill(password);
+        await this.btnLogin.click();
+    }
+
+    public async signOut() {
+        await Promise.all([
+            this.page.waitForLoadState(),
+            this.loginUsername.click(),
+            this.signOutText.click(),
+            this.page.waitForLoadState(),
+            expect((await this.signIn.innerText()).toString()).toContain('Sign in')])
+    }
+
+    public async adLogin(username: string, password: string) {
+        await this.adUsername.fill(username);
+        await Promise.all([
+            this.page.waitForNavigation(),
+            this.adNext.click()
+        ]);
+        await Promise.all([
+            this.adPassword.fill(password),
+        ]);
+        await Promise.all([
+            this.page.waitForNavigation(),
+            this.login.click()
+        ]);
+
+    }
+
+    public async adSignOut() {
+        await this.adUserNameDropdown.click();
+        expect(await this.adSignOutText.innerText()).toContain('Sign out')
+    }
+
+    public async adUnathorisedDetails() {
+        expect(await this.adUnathorisedError.innerText()).toContain("Sorry, you do not have access to this website.");
+    }
+
+    public async adPasswordErrorCheck() {
+        expect(await this.adPasswordError.innerText()).toContain('Your account or password is incorrect');
+    }
 }
-
-
-
-
