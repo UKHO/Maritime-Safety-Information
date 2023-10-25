45ba2c39ac89ab6f02ba3ae9463355e0e40481a7
Martyn Fewtrell <amartyn@engineering.ukho.gov.uk>
2023-10-25 11:20:42 +0100

    anotate config file

--- Tests/playwright.config.ts
+++ Tests/playwright.config.ts
@@ -3,29 +3,43 @@ import { devices } from '@playwright/test';
 
 
 const config: PlaywrightTestConfig = {
-  testDir: './AutoTests',
-  timeout: 100 * 1000,
-  expect: {
-    timeout: 10000
-  },
-  globalTimeout: 60 * 60 * 1000,
-  forbidOnly: !!process.env.CI,
 
-  retries: 3,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: [ ['junit', { outputFile: 'results.xml' }] ],
-  use: {
-    actionTimeout: 10 * 1000,
-    navigationTimeout: 150 * 1000,
-    trace: 'on-first-retry',
-    
-  },
-  projects: [
+    retries: 3,
+    testDir: './AutoTests',
+
+    /* Maximum time one test can run for. */
+    timeout: 100 * 1000,
+
+    /*  Maximum time expect() should wait for the condition to be met. */
+    expect: {
+        timeout: 10 * 1000
+    },
+
+    /* Sets action and navigation timeouts */
+    use: {
+        actionTimeout: 10 * 1000,
+        navigationTimeout: 30 * 1000,
+        trace: 'on-first-retry',
+    },
+
+    /* Maximum time for the entire test run */
+    globalTimeout: 60 * 60 * 1000,
+
+    /* control the maximum number of parallel worker processes - setting to one disables parallelism */
+    workers: process.env.CI ? 1 : undefined,
+
+    /* exit with an error if any tests or groups are marked as test.only() or test.describe.only() */
+    forbidOnly: !!process.env.CI,
+
+    /* aggregates results into usable format */
+    reporter: [['junit', { outputFile: 'results.xml' }]],
+
+    /* Manage which browsers toi test against */
+    projects: [
     {
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        
       },
     },
 
