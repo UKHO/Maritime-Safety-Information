aaa6d6a6ac492cd3a2e7dfd68518bbe525656ce9
HugoBurgess <hugo.burgess@ukho.gov.uk>
2024-07-10 14:13:51 +0100

    Testing

--- Tests/AutoTests/FunctionalTest/functional-Dailyfiledownload.spec.ts
+++ Tests/AutoTests/FunctionalTest/functional-Dailyfiledownload.spec.ts
@@ -6,31 +6,31 @@ import noticeToMarinerWeekDownload from '../../pageObject/noticeToMarine-weekfil
 import noticeToMariner from '../../pageObject/noticetomarine.page';
 
 test.describe("Goto maritime-safety-information Notice To Mariner Page to Check The Weekly and Daily File Download", () => {
-  let noticeFileDownload: noticeToMarinerWeekDownload;
-  let notice:noticeToMariner;
-  let login:Login;
-  test.beforeEach(async ({ page }) => {
+    let noticeFileDownload: noticeToMarinerWeekDownload;
+    let notice: noticeToMariner;
+    let login: Login;
+    test.beforeEach(async ({ page }) => {
 
-      await page.goto(app.url);
-      noticeFileDownload = new noticeToMarinerWeekDownload(page);
-      notice = new noticeToMariner(page);
-      login=new Login(page);
-  });
+        await page.goto(app.url);
+        noticeFileDownload = new noticeToMarinerWeekDownload(page);
+        notice = new noticeToMariner(page);
+        login = new Login(page);
+    });
 
-  test('Should Goto Notices to Mariner Page for Daily download File', async ({ page, context }) => {
-    await noticeFileDownload.goToNoticeToMariner();
-    await noticeFileDownload.goToDailyFile();
-    await noticeFileDownload.checkDailyFileDownload();
-  })
+    test('Should Goto Notices to Mariner Page for Daily download File', async ({ page, context }) => {
+        await noticeFileDownload.goToNoticeToMariner();
+        await noticeFileDownload.goToDailyFile();
+        await noticeFileDownload.checkDailyFileDownload();
+    })
 
-  test('Should Goto Notices to Mariner Page for Daily download File with Distributor Login', async ({ page, context }) => {
-  await login.goToSignIn();
-  await login.loginWithDistributorDetails(app.DistributorTest_UserName, app.DistributorTest_Password);
-  await noticeFileDownload.goToNoticeToMariner();
-  await noticeFileDownload.goToDailyFile();
-  await noticeFileDownload.checkDailyFileName();
-  await noticeFileDownload.checkDailyFileSize();
-  await noticeFileDownload.checkDailyFileDownload();
-  await noticeFileDownload.checkDailyWeekFileName()
-})
+    test('Should Goto Notices to Mariner Page for Daily download File with Distributor Login', async ({ page, context }) => {
+        await login.goToSignIn();
+        await login.loginWithDistributorDetails(app.DistributorTest_UserName, app.DistributorTest_Password);
+        await noticeFileDownload.goToNoticeToMariner();
+        await noticeFileDownload.goToDailyFile();
+        await noticeFileDownload.checkDailyFileName();
+        await noticeFileDownload.checkDailyFileSize();
+        await noticeFileDownload.checkDailyFileDownload();
+        await noticeFileDownload.checkDailyWeekFileName()
+    })
 });
