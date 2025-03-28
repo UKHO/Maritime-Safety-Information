c0e0c1a0871e9f63b26598dbda19f1569d5258bb
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-03-28 18:14:46 +0000

    Add Azure Storage and SQL Server support
    Updated `Program.cs` to include Azure Storage with emulator support and table storage configuration. Added references to a mock container and SQL Server database.
    
    Modified `UKHO.MaritimeSafetyInformation.Web.AppHost.csproj` to include new package references for `Aspire.Hosting.Azure.Storage` and `Aspire.Hosting.SqlServer` (both version 9.1.0), along with a project reference to `UKHO.MaritimeSafetyInformation.Common`.

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web.AppHost/Program.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web.AppHost/Program.cs
@@ -4,9 +4,19 @@ var mockcontainer = builder.AddDockerfile("adds-mock", @"..\UKHO.ADDS.Mocks\src\
     .WithHttpEndpoint(port: 5000, targetPort: 5678, "mock-endpoint");
 var mockEndpoint = mockcontainer.GetEndpoint("mock-endpoint");
 
+var storage = builder.AddAzureStorage("storageConnection").RunAsEmulator(
+                        azurite =>
+                        {
+                            azurite.WithDataVolume();
+                        });
+
+var tableStorage = storage.AddTables("fss-tables-connection");
+
 var rnwDb = builder.AddSqlServer("sql")
     .WithDataVolume()
     .AddDatabase("RadioNavigationalWarningsDb");
+
+
 
 builder.AddProject<Projects.UKHO_MaritimeSafetyInformation_Web>("ukho-msi-web")
     .WithReference(mockEndpoint)
