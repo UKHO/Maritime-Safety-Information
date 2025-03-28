d47557e4687abe6b2edb5ccd77c5c78e73c25a2b
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-03-28 12:34:32 +0000

    Add mock container and endpoint for testing
    Updated `Program.cs` to include a mock container with a Dockerfile and a mock endpoint. Configured the application to reference the mock endpoint and a SQL Server database for `RadioNavigationalWarningsDb`. Added a new project for the mock service in the solution file with necessary build configurations.

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web.AppHost/Program.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Web.AppHost/Program.cs
@@ -1,10 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var mockcontainer = builder.AddDockerfile("adds-mock", @"..\UKHO.ADDS.Mocks\src\ADDSMock")
+    .WithHttpEndpoint(port: 5000, targetPort: 5678, "mock-endpoint");
+var mockEndpoint = mockcontainer.GetEndpoint("mock-endpoint");
+
 var rnwDb = builder.AddSqlServer("sql")
     .WithDataVolume()
     .AddDatabase("RadioNavigationalWarningsDb");
 
 builder.AddProject<Projects.UKHO_MaritimeSafetyInformation_Web>("ukho-msi-web")
+    .WithReference(mockEndpoint)
+    .WaitFor(mockcontainer)
     .WithReference(rnwDb)
     .WaitFor(rnwDb);
 
