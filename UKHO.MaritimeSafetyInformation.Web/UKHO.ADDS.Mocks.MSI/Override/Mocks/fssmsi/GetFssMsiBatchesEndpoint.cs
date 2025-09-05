b45b382d5bfaaadd8a33be11d979b1278d2f1898
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-09-05 13:56:41 +0100

    Add FSS MSI Override batch attributes and batches endpoints
    Introduce `GetFssMsiBatchAttributesEndpoint` and
    `GetFssMsiBatchesEndpoint` classes to handle HTTP GET
    requests for batch attributes and batches. The
    `/attributes/search` endpoint serves `attributes.json`,
    while the `/batch` endpoint serves `annualfiles.json`.
    Both endpoints include state handling and metadata for
    documentation.

---
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.ADDS.Mocks.MSI/Override/Mocks/fssmsi/GetFssMsiBatchesEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using UKHO.ADDS.Mocks.Markdown;
+using UKHO.ADDS.Mocks.Mime;
+using UKHO.ADDS.Mocks.States;
+
+namespace UKHO.ADDS.Mocks.Override.Mocks.fssmsi
+{
+    public class GetFssMsiBatchesEndpoint : ServiceEndpointMock
+    {
+        public override void RegisterSingleEndpoint(IEndpointMock endpoint) =>
+            endpoint.MapGet("/batch", (HttpRequest request) =>
+                {
+                    var state = GetState(request);
+
+                    switch (state)
+                    {
+                        case WellKnownState.Default:
+                            try
+                            {
+                                var fs = GetFileSystem();
+                                return Results.File(fs.OpenFile("/annualfiles.json", FileMode.Open, FileAccess.Read), MimeType.Application.Json);
+                            }
+                            catch (Exception)
+                            {
+                                return Results.NotFound("Could not find the path in the /files GET method");
+                            }
+
+                        default:
+                            // Just send default responses
+                            return WellKnownStateHandler.HandleWellKnownState(state);
+                    }
+                })
+                .Produces<string>()
+                .WithEndpointMetadata(endpoint, d =>
+                {
+                    d.Append(new MarkdownHeader("Gets Batches (MSI)", 3));
+                    d.Append(new MarkdownParagraph("This is driven from a static file annualfiles.json"));
+                });
+    }
+}
