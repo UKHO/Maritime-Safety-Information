e978f399de50c3c2d1c299f1570966c49844fbd3
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-10-30 15:15:53 +0000

    Add FileShare Service (MSI) to service registry
    Updated `Program.cs` to register a new service definition for the "FileShare Service (MSI)" in the `ServiceRegistry`. Added necessary `using` directives for `UKHO.ADDS.Mocks.Configuration` and `UKHO.ADDS.Mocks.Domain.Configuration`. This change enhances the mock server by introducing support for the new service.

--- UKHO.MaritimeSafetyInformation.Web/UKHO.ADDS.Mocks.MSI/Program.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.ADDS.Mocks.MSI/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace UKHO.ADDS.Mocks.MSI
+﻿using UKHO.ADDS.Mocks.Configuration;
+using UKHO.ADDS.Mocks.Domain.Configuration;
+
+namespace UKHO.ADDS.Mocks.MSI
 {
     internal class Program
     {
         public static async Task Main(string[] args)
         {
             MockServices.AddServices();
+            ServiceRegistry.AddDefinition(new ServiceDefinition("fssmsi", "FileShare Service (MSI)", []));
 
             await MockServer.RunAsync(args);
         }
