c1068f2ec2b3282390f055362a23baefb61329dd
hukho <128484816+hukho@users.noreply.github.com>
2026-03-03 13:41:03 +0000

    Remove CSP for testing

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Extensions/RequestPipelineConfigExtensions.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Extensions/RequestPipelineConfigExtensions.cs
@@ -21,62 +21,66 @@ namespace UKHO.MaritimeSafetyInformation.Common.Extensions
             app.UseHttpsRedirection();
             app.UseHsts(x => x.MaxAge(365).IncludeSubdomains());
             app.UseReferrerPolicy(x => x.NoReferrer());
-            app.UseCsp(x =>
-            {
-                x.DefaultSources(y => y.Self());
-                x.ScriptSources(y => y.Self().StrictDynamic().CustomSources(
-#if DEBUG
-                    "https://localhost:*",
-                    "http://localhost:*",
-#endif
-                    "https://www.googletagmanager.com"
-                ));
-                x.StyleSources(y => y.Self().UnsafeInline().CustomSources(
-                    "https://unpkg.com",
-                    "https://cdn.jsdelivr.net"
-                ));
-                x.FontSources(y => y.Self().CustomSources(
-                    "https://unpkg.com",
-                    "https://cdn.jsdelivr.net"
-                ));
-                x.ImageSources(y => y.Self().CustomSources(
-                    "data:",
-                    "https://www.googletagmanager.com",
-                    "https://www.google.com",
-                    "https://www.google.co.uk",
-                    "https://track-eu1.hubspot.com",
-                    "https://perf-eu1.hsforms.com",
-                    "https://px.ads.linkedin.com",
-                    "https://bat.bing.com",
-                    "https://*.svc.dynamics.com"
-                ));
-                x.ConnectSources(y => y.Self().CustomSources(
-#if DEBUG
-                    "https://localhost:*",
-                    "http://localhost:*",
-                    "ws://localhost:*",
-                    "wss://localhost:*",
-                    "http://127.0.0.1:*",
-                    "https://127.0.0.1:*",
-                    "ws://127.0.0.1:*",
-                    "wss://127.0.0.1:*",
-#endif
-                    "https://www.googletagmanager.com",
-                    "https://www.google.com",
-                    "https://region1.google-analytics.com",
-                    "https://cdn-ukwest.onetrust.com",
-                    "https://privacyportal-uk.onetrust.com",
-                    "https://api-eu1.hubapi.com",
-                    "https://cta-eu1.hubspot.com",
-                    "https://static.hsappstatic.net",
-                    "https://px.ads.linkedin.com",
-                    "https://pagead2.googlesyndication.com",
-                    "https://bat.bing.com"
-                ));
-                x.FrameSources(y => y.Self().CustomSources(
-                    "https://www.googletagmanager.com"
-                ));
-            });
+//            app.UseCsp(x =>
+//            {
+//                x.DefaultSources(y => y.Self());
+//                x.ScriptSources(y => y.Self().StrictDynamic().CustomSources(
+//#if DEBUG
+//                    "https://localhost:*",
+//                    "http://localhost:*",
+//#endif
+//                    "https://www.googletagmanager.com",
+//                    "https://*.hubspot.com",
+//                    "https://*.hs-scripts.com"
+//                ));
+//                x.StyleSources(y => y.Self().UnsafeInline().CustomSources(
+//                    "https://unpkg.com",
+//                    "https://cdn.jsdelivr.net"
+//                ));
+//                x.FontSources(y => y.Self().CustomSources(
+//                    "https://unpkg.com",
+//                    "https://cdn.jsdelivr.net"
+//                ));
+//                x.ImageSources(y => y.Self().CustomSources(
+//                    "data:",
+//                    "https://www.googletagmanager.com",
+//                    "https://www.google.com",
+//                    "https://www.google.co.uk",
+//                    "https://*.hubspot.com",
+//                    "https://perf-eu1.hsforms.com",
+//                    "https://px.ads.linkedin.com",
+//                    "https://bat.bing.com",
+//                    "https://*.svc.dynamics.com"
+//                ));
+//                x.ConnectSources(y => y.Self().CustomSources(
+//#if DEBUG
+//                    "https://localhost:*",
+//                    "http://localhost:*",
+//                    "ws://localhost:*",
+//                    "wss://localhost:*",
+//                    "http://127.0.0.1:*",
+//                    "https://127.0.0.1:*",
+//                    "ws://127.0.0.1:*",
+//                    "wss://127.0.0.1:*",
+//#endif
+//                    "https://www.googletagmanager.com",
+//                    "https://www.google.com",
+//                    "https://region1.google-analytics.com",
+//                    "https://cdn-ukwest.onetrust.com",
+//                    "https://privacyportal-uk.onetrust.com",
+//                    "https://api-eu1.hubapi.com",
+//                    "https://static.hsappstatic.net",
+//                    "https://px.ads.linkedin.com",
+//                    "https://pagead2.googlesyndication.com",
+//                    "https://bat.bing.com",
+//                    "https://vc.hotjar.io",
+//                    "https://*.hubspot.com",
+//                    "https://*.hs-scripts.com"
+//                ));
+//                x.FrameSources(y => y.Self().CustomSources(
+//                    "https://www.googletagmanager.com"
+//                ));
+//            });
             app.UseCustomSecurityHeaders();
             app.UseStaticFiles();
             app.UseXfo(x => x.SameOrigin());
