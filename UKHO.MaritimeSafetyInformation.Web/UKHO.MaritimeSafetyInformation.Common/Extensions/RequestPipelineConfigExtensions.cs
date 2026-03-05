80254c61dec12b59546cfe1af73026b8c517343a
hukho <128484816+hukho@users.noreply.github.com>
2026-03-05 11:24:18 +0000

    Deploy to QA

--- UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Extensions/RequestPipelineConfigExtensions.cs
+++ UKHO.MaritimeSafetyInformation.Web/UKHO.MaritimeSafetyInformation.Common/Extensions/RequestPipelineConfigExtensions.cs
@@ -26,62 +26,6 @@ namespace UKHO.MaritimeSafetyInformation.Common.Extensions
                 x.ScriptSources(y => y.Self().StrictDynamic());
                 x.ObjectSources(y => y.None());
                 x.BaseUris(y => y.None());
-                //                x.DefaultSources(y => y.Self());
-                //                x.ScriptSources(y => y.Self().StrictDynamic().CustomSources(
-                //#if DEBUG
-                //                    "https://localhost:*",
-                //                    "http://localhost:*",
-                //#endif
-                //                    "https://*.googletagmanager.com",
-                //                    "https://*.hubspot.com",
-                //                    "https://*.hs-scripts.com"
-                //                ));
-                //                x.ConnectSources(y => y.Self().CustomSources(
-                //#if DEBUG
-                //                    "https://localhost:*",
-                //                    "http://localhost:*",
-                //                    "ws://localhost:*",
-                //                    "wss://localhost:*",
-                //                    "http://127.0.0.1:*",
-                //                    "https://127.0.0.1:*",
-                //                    "ws://127.0.0.1:*",
-                //                    "wss://127.0.0.1:*",
-                //#endif
-                //                    "www.google.com",
-                //                    "https://*.google-analytics.com",
-                //                    "https://*.analytics.google.com",
-                //                    "https://*.googletagmanager.com",
-                //                    "https://cdn-ukwest.onetrust.com",
-                //                    "https://api-eu1.hubapi.com",
-                //                    "https://px.ads.linkedin.com",
-                //                    "https://pagead2.googlesyndication.com",
-                //                    "https://privacyportal-uk.onetrust.com",
-                //                    "https://region1.google-analytics.com",
-                //                    "https://*.hubspot.com",
-                //                    "https://*.hs-scripts.com"
-                //                ));
-                //                x.ImageSources(y => y.Self().CustomSources(
-                //                    "data:",
-                //                    "www.google.com",
-                //                    "www.google.co.uk",
-                //                    "https://*.google-analytics.com",
-                //                    "https://*.googletagmanager.com",
-                //                    "https://*.linkedin.com",
-                //                    "https://*.hubspot.com",
-                //                    "https://perf-eu1.hsforms.com",
-                //                    "https://cdn-ukwest.onetrust.com"
-                //                ));
-                //                x.StyleSources(y => y.Self().UnsafeInline().CustomSources(
-                //                    "https://unpkg.com",
-                //                    "https://cdn.jsdelivr.net"
-                //                ));
-                //                x.FontSources(y => y.Self().CustomSources(
-                //                    "https://cdn.jsdelivr.net",
-                //                    "https://unpkg.com"
-                //                ));
-                //                x.FrameSources(y => y.Self().CustomSources(
-                //                    "https://www.googletagmanager.com"
-                //                ));
             });
             app.UseCustomSecurityHeaders();
             app.UseStaticFiles();
